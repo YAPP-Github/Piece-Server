2ef6baf419d1e07c0d5ffe714fc81f35f51ad650
Chanho lee <devchlee12@gmail.com>
2024-12-11 20:41:20 +0900

    feat : Google Oauth Provider/Client 구현

---
+++ api/src/main/java/org/yapp/domain/auth/application/oauth/google/GoogleOauthClient.java
@@ -0,0 +1,31 @@
+package org.yapp.domain.auth.application.oauth.google;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.yapp.domain.auth.application.oauth.OauthClient;
+
+@Component
+public class GoogleOauthClient implements OauthClient {
+    @Value("${oauth.googleUserInfoUri}")
+    private String userInfoUri;
+
+    @Override
+    public String getOAuthProviderUserId(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        HttpEntity<Object> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<JsonNode> response = new RestTemplate().exchange(
+                userInfoUri, HttpMethod.GET, entity, JsonNode.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody().get("sub").asText();
+        }
+        throw new RuntimeException();
+    }
+}
