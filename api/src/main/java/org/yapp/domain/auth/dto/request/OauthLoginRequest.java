a96596c1d6770bbf148a103d8671477b70b81ac3
Chanho lee <devchlee12@gmail.com>
2024-12-15 23:30:02 +0900

    feat: 테스트 코드를 위한 DTO 생성자 추가

--- api/src/main/java/org/yapp/domain/auth/dto/request/OauthLoginRequest.java
+++ api/src/main/java/org/yapp/domain/auth/dto/request/OauthLoginRequest.java
@@ -1,11 +1,13 @@
 package org.yapp.domain.auth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class OauthLoginRequest {
-    private String providerName;
-    private String token;
+  private String providerName;
+  private String token;
 }
