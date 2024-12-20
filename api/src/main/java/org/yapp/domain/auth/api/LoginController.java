bfef058c120ef3776ed13cec6b26d356ae8e9e99
Chanho lee <devchlee12@gmail.com>
2024-12-20 23:38:00 +0900

    fix: 로그인 컨트롤러에 RequestBody 없던 문제 수정

--- api/src/main/java/org/yapp/domain/auth/api/LoginController.java
+++ api/src/main/java/org/yapp/domain/auth/api/LoginController.java
@@ -1,24 +1,26 @@
 package org.yapp.domain.auth.api;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yapp.domain.auth.application.oauth.service.OauthService;
 import org.yapp.domain.auth.dto.request.OauthLoginRequest;
 import org.yapp.domain.auth.dto.response.OauthLoginResponse;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
-    private final OauthService oauthService;
+  private final OauthService oauthService;
 
-    @PostMapping("/oauth")
-    public ResponseEntity<OauthLoginResponse> oauthLogin(OauthLoginRequest request){
-        OauthLoginResponse response = oauthService.login(request);
-        return ResponseEntity.ok(response);
-    }
+  @PostMapping("/oauth")
+  public ResponseEntity<OauthLoginResponse> oauthLogin(@RequestBody OauthLoginRequest request) {
+    OauthLoginResponse response = oauthService.login(request);
+    return ResponseEntity.ok(response);
+  }
 
 }
