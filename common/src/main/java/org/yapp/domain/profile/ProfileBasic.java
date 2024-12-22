71784edefd1665d4263bfdba6e49830a1918a3c1
lujaeC <youjae0803@gmail.com>
2024-12-22 20:47:35 +0900

    feat: 프로필 도메인 설계

---
+++ common/src/main/java/org/yapp/domain/profile/ProfileBasic.java
@@ -0,0 +1,48 @@
+package org.yapp.domain.profile;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class ProfileBasic {
+  @Column(name = "nickname", nullable = false)
+  private String nickname;
+
+  @Column(name = "birthdate")
+  private Date birthdate;
+
+  @Column(name = "height")
+  private int height;
+
+  @Column(name = "job")
+  private String job;
+
+  @Column(name = "location")
+  private String location;
+
+  @Column(name = "smoking_status")
+  private String smokingStatus;
+
+  @Column(name = "religion")
+  private String religion;
+
+  @Column(name = "sns_activity_level")
+  private String snsActivityLevel;
+
+  @Column(name = "phone_number", nullable = false)
+  private String phoneNumber;
+
+  @Column(name = "image_url")
+  private String imageUrl;
+}
---
+++ common/src/main/java/org/yapp/domain/profile/ValueItem.java
@@ -0,0 +1,40 @@
+package org.yapp.domain.profile;
+
+import org.hibernate.annotations.Type;
+
+import java.util.Map;
+
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "value_item")
+@Getter
+@NoArgsConstructor
+public class ValueItem {
+  @Id
+  @Column(name = "value_item_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String question;
+
+  @Type(JsonType.class)
+  @Column(name = "answers", columnDefinition = "longtext", nullable = false)
+  private Map<Integer, Object> answers;
+
+  @Builder
+  public ValueItem(String question, Map<Integer, Object> answers) {
+    this.question = question;
+    this.answers = answers;
+  }
+}
