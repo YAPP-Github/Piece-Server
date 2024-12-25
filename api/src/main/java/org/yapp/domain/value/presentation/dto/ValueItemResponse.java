f7a7bf018515e7743ff1b18f310d6cd779d18ff4
lujaeC <youjae0803@gmail.com>
2024-12-25 21:54:57 +0900

    [PC-177] feat: ValeItem에 Category 필드 추가

--- api/src/main/java/org/yapp/domain/value/presentation/dto/ValueItemResponse.java
+++ api/src/main/java/org/yapp/domain/value/presentation/dto/ValueItemResponse.java
@@ -4,8 +4,9 @@ import org.yapp.domain.value.ValueItem;
 
 import java.util.Map;
 
-public record ValueItemResponse(Long id, String question, Map<Integer, Object> answers) {
+public record ValueItemResponse(Long id, String category, String question, Map<Integer, Object> answers) {
   public static ValueItemResponse from(ValueItem valueItem) {
-    return new ValueItemResponse(valueItem.getId(), valueItem.getQuestion(), valueItem.getAnswers());
+    return new ValueItemResponse(valueItem.getId(), valueItem.getCategory(), valueItem.getQuestion(),
+        valueItem.getAnswers());
   }
 }
--- common/src/main/java/org/yapp/domain/value/ValueItem.java
+++ common/src/main/java/org/yapp/domain/value/ValueItem.java
@@ -25,6 +25,9 @@ public class ValueItem {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column
+  private String category;
+
   @Column(nullable = false)
   private String question;
 
