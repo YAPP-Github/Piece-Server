b6aa34ff787a7aaf6ca7c04c3782a44c408ff875
Chanho lee <devchlee12@gmail.com>
2024-12-28 16:32:41 +0900

    feat: GoogleIdTokenVerifier Bean 등록

--- api/src/main/java/org/yapp/domain/auth/application/oauth/google/GoogleOauthClient.java
+++ api/src/main/java/org/yapp/domain/auth/application/oauth/google/GoogleOauthClient.java
@@ -2,10 +2,7 @@ package org.yapp.domain.auth.application.oauth.google;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.yapp.domain.auth.application.oauth.OauthClient;
 import org.yapp.error.code.auth.AuthErrorCode;
@@ -13,25 +10,20 @@ import org.yapp.error.exception.ApplicationException;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class GoogleOauthClient implements OauthClient {
-  @Value("${app.google.client.id}")
-  private String googleClientId;
+  private final GoogleIdTokenVerifier googleIdTokenVerifier;
 
   @Override
   public String getOAuthProviderUserId(String token) {
     GoogleIdToken googleIdToken;
 
-    GoogleIdTokenVerifier verifier =
-        new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance()).setAudience(
-            Collections.singletonList(googleClientId)).build();
     try {
-      googleIdToken = verifier.verify(token);
+      googleIdToken = googleIdTokenVerifier.verify(token);
     } catch (GeneralSecurityException e) {
       throw new ApplicationException(AuthErrorCode.OAUTH_ERROR);
     } catch (IOException e) {
