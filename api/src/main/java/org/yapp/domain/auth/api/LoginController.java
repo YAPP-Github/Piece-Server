6ade58785641b7e0d4e029a32cdba628e5c08932
Chanho lee <devchlee12@gmail.com>
2024-12-28 15:39:46 +0900

    fix: ApiResponse 이름 변경 -> CommonResponse

--- api/src/main/java/org/yapp/domain/auth/api/LoginController.java
+++ api/src/main/java/org/yapp/domain/auth/api/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.yapp.domain.auth.application.oauth.service.OauthService;
 import org.yapp.domain.auth.dto.request.OauthLoginRequest;
 import org.yapp.domain.auth.dto.response.OauthLoginResponse;
-import org.yapp.util.ApiResponse;
+import org.yapp.util.CommonResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,18 +19,18 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
   private final OauthService oauthService;
 
-  @PostMapping("/oauth")
-  public ResponseEntity<ApiResponse<OauthLoginResponse>> oauthLogin(@RequestBody OauthLoginRequest request) {
-    OauthLoginResponse response = oauthService.login(request);
-    return ResponseEntity.ok(ApiResponse.createSuccess(response));
-  }
-
   /**
    * 개발중 소셜 로그인으로 사용자으 accessToken을 가져오기 어렵기 때문에 만든 임시 메서드
    */
   @PostMapping("/test/users/{userId}")
-  public ResponseEntity<ApiResponse<OauthLoginResponse>> getToken(@PathVariable Long userId) {
+  public ResponseEntity<CommonResponse<OauthLoginResponse>> getToken(@PathVariable Long userId) {
     OauthLoginResponse response = oauthService.tmpTokenGet(userId);
-    return ResponseEntity.ok(ApiResponse.createSuccess(response));
+    return ResponseEntity.ok(CommonResponse.createSuccess(response));
+  }
+
+  @PostMapping("/oauth")
+  public ResponseEntity<CommonResponse<OauthLoginResponse>> oauthLogin(@RequestBody OauthLoginRequest request) {
+    OauthLoginResponse response = oauthService.login(request);
+    return ResponseEntity.ok(CommonResponse.createSuccess(response));
   }
 }
