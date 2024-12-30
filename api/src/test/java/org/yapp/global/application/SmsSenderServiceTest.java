c6c20bf90231cef4847e87216795345a0b1d3a20
Chanho lee <devchlee12@gmail.com>
2024-12-31 00:17:08 +0900

    [PC-106] test: SmsSenderService 단위 테스트

---
+++ api/src/test/java/org/yapp/global/application/SmsSenderServiceTest.java
@@ -0,0 +1,62 @@
+package org.yapp.global.application;
+
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class SmsSenderServiceTest {
+  private SmsSenderService smsSenderService;
+
+  @Mock
+  private DefaultMessageService messageService;
+
+  @Value("${coolsms.apikey}")
+  private String apiKey = "mock-api-key";
+
+  @Value("${coolsms.apisecret}")
+  private String apiSecret = "mock-api-secret";
+
+  @Value("${coolsms.fromnumber}")
+  private String fromNumber = "01012345678";
+
+  @BeforeEach
+  void setUp() {
+    smsSenderService = new SmsSenderService();
+    smsSenderService.initForTest(apiKey, apiSecret, fromNumber, messageService);
+  }
+
+  @Test
+  @DisplayName("원하는 SMS 메시지를 원하는 곳으로 전송한다.")
+  void shouldSendSmsMessageCorrectly() {
+    // Given
+    String to = "01098765432";
+    String smsMessage = "Test Message";
+
+    // When
+    smsSenderService.sendSMS(to, smsMessage);
+
+    // Then
+    ArgumentCaptor<SingleMessageSendingRequest> messageCaptor =
+        ArgumentCaptor.forClass(SingleMessageSendingRequest.class);
+    Mockito.verify(messageService, times(1)).sendOne(messageCaptor.capture());
+
+    Message capturedMessage = messageCaptor.getValue().getMessage();
+    assertThat(capturedMessage.getFrom()).isEqualTo(fromNumber);
+    assertThat(capturedMessage.getTo()).isEqualTo(to);
+    assertThat(capturedMessage.getText()).isEqualTo(smsMessage);
+  }
+}
