28e989ae3176a637510b471b67a4fbedf96030bd
Chanho lee <devchlee12@gmail.com>
2025-01-04 01:00:01 +0900

    [PC-214] feat: 유저 Role에 따른 로그인 분기 로직 추가

--- api/src/main/java/org/yapp/domain/auth/application/oauth/service/OauthService.java
+++ api/src/main/java/org/yapp/domain/auth/application/oauth/service/OauthService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.yapp.domain.auth.application.jwt.JwtUtil;
 import org.yapp.domain.auth.application.oauth.OauthProvider;
 import org.yapp.domain.auth.application.oauth.OauthProviderResolver;
+import org.yapp.domain.auth.presentation.dto.enums.RoleStatus;
 import org.yapp.domain.auth.presentation.dto.request.OauthLoginRequest;
 import org.yapp.domain.auth.presentation.dto.response.OauthLoginResponse;
 import org.yapp.domain.user.User;
@@ -27,21 +28,33 @@ public class OauthService {
     //이미 가입된 유저인지 확인하고 가입되어 있지 않으면 회원가입 처리
     Optional<User> userOptional = userRepository.findByOauthId(oauthId);
     if (userOptional.isEmpty()) {
-      User newUser = User.builder().oauthId(oauthId).build();
+      User newUser = User.builder().oauthId(oauthId).role(RoleStatus.NONE.getStatus()).build();
       User savedUser = userRepository.save(newUser);
       Long userId = savedUser.getId();
-      String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, "USER", 600000L);
-      String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, "USER", 864000000L);
-      return new OauthLoginResponse(true, accessToken, refreshToken);
+      String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, "NONE", 600000L);
+      String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, "NONE", 864000000L);
+      return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
     }
 
     //이미 가입한 유저인 경우 로그인 처리
     Long userId = userOptional.get().getId();
     final User user = userOptional.get();
+
     String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, user.getRole(), 600000L);
     String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, user.getRole(), 864000000L);
 
-    return new OauthLoginResponse(false, accessToken, refreshToken);
+    //아직 SMS 인증이 완료되지 않음
+    if (user.getRole().equals("NONE")) {
+      return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
+    }
+
+    // SMS 인증은 완료되었으나, 프로필 등록을 안함
+    if (user.getRole().equals("REGISTER")) {
+      return new OauthLoginResponse(RoleStatus.REGISTER.getStatus(), accessToken, refreshToken);
+    }
+
+    // 가입과 프로필 등록까지 완료된 유저
+    return new OauthLoginResponse(user.getRole(), accessToken, refreshToken);
   }
 
 
@@ -56,6 +69,6 @@ public class OauthService {
     String accessToken = jwtUtil.createJwt("access_token", userId, user.getOauthId(), user.getRole(), 600000L);
     String refreshToken = jwtUtil.createJwt("refresh_token", userId, user.getOauthId(), user.getRole(), 864000000L);
 
-    return new OauthLoginResponse(false, accessToken, refreshToken);
+    return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
   }
 }
