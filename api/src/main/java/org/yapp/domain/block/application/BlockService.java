faeaec8de9bbfd7c1f59563f6cb15acd3f912016
LujaeC <youjae0803@gmail.com>
2025-01-05 10:48:59 +0900

    [PC-293] feat: 핸드폰 번호로 Block 추가

---
+++ api/src/main/java/org/yapp/domain/block/application/BlockService.java
@@ -0,0 +1,44 @@
+package org.yapp.domain.block.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yapp.domain.block.Block;
+import org.yapp.domain.block.application.dto.BlockCreateDto;
+import org.yapp.domain.block.dao.BlockRepository;
+import org.yapp.domain.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BlockService {
+    private final BlockRepository blockRepository;
+
+    @Transactional()
+    public void blockPhoneNumbers(BlockCreateDto blockCreateDto) {
+        Long userId = blockCreateDto.userId();
+        List<String> phoneNumbers = blockCreateDto.phoneNumbers();
+        List<Block> newBlocks = new ArrayList<>();
+
+        Set<String> blockedPhoneNumbers = blockRepository.findBlocksByUserId(userId)
+                .stream()
+                .map(Block::getPhoneNumber)
+                .collect(Collectors.toSet());
+
+        phoneNumbers.stream()
+                .filter(phoneNumber -> !blockedPhoneNumbers.contains(phoneNumber))
+                .forEach(phoneNumber -> {
+                    Block block = Block.builder()
+                            .user(User.builder().id(userId).build())
+                            .phoneNumber(phoneNumber)
+                            .build();
+                    newBlocks.add(block);
+                });
+
+        blockRepository.saveAll(newBlocks);
+    }
+}
