18281a916bc76dddf5337ad428560b1a64f4962e
LujaeC <youjae0803@gmail.com>
2025-01-07 01:19:10 +0900

    [PC-297] refactor: 코드 포맷팅

--- api/src/main/java/org/yapp/domain/profile/application/ProfileService.java
+++ api/src/main/java/org/yapp/domain/profile/application/ProfileService.java
@@ -1,14 +1,18 @@
 package org.yapp.domain.profile.application;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.yapp.domain.profile.Profile;
 import org.yapp.domain.profile.ProfileBasic;
 import org.yapp.domain.profile.ProfileBio;
 import org.yapp.domain.profile.ProfileValuePick;
-import org.yapp.domain.profile.presentation.request.ProfileUpdateRequest;
 import org.yapp.domain.profile.application.dto.ProfileCreateDto;
 import org.yapp.domain.profile.dao.ProfileRepository;
+import org.yapp.domain.profile.presentation.request.ProfileUpdateRequest;
 import org.yapp.domain.profile.presentation.request.ProfileValuePickPair;
 import org.yapp.domain.profile.presentation.request.ProfileValuePickUpdateRequest;
 import org.yapp.domain.user.User;
@@ -16,76 +20,74 @@ import org.yapp.domain.user.application.UserService;
 import org.yapp.error.dto.ProfileErrorCode;
 import org.yapp.error.exception.ApplicationException;
 
-import lombok.RequiredArgsConstructor;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
-  private final UserService userService;
-  private final ProfileValueService profileValueService;
-  private final ProfileRepository profileRepository;
 
-  @Transactional
-  public Profile create(ProfileCreateDto dto) {
-    ProfileBasic profileBasic = ProfileBasic.builder().nickname(dto.nickName()).phoneNumber(dto.phoneNumber()).build();
-    ProfileBio profileBio = ProfileBio.builder().build();
+    private final UserService userService;
+    private final ProfileValueService profileValueService;
+    private final ProfileRepository profileRepository;
 
-    Profile profile = Profile.builder().profileBasic(profileBasic).profileBio(profileBio).build();
-    Profile savedProfile = profileRepository.save(profile);
-    List<ProfileValuePick> allProfileValuePicks = profileValueService.createAllProfileValues(savedProfile.getId());
-    savedProfile.updateProfileValues(allProfileValuePicks);
+    @Transactional
+    public Profile create(ProfileCreateDto dto) {
+        ProfileBasic profileBasic = ProfileBasic.builder().nickname(dto.nickName())
+            .phoneNumber(dto.phoneNumber()).build();
+        ProfileBio profileBio = ProfileBio.builder().build();
 
-    return savedProfile;
-  }
+        Profile profile = Profile.builder().profileBasic(profileBasic).profileBio(profileBio)
+            .build();
+        Profile savedProfile = profileRepository.save(profile);
+        List<ProfileValuePick> allProfileValuePicks = profileValueService.createAllProfileValues(
+            savedProfile.getId());
+        savedProfile.updateProfileValues(allProfileValuePicks);
 
-  @Transactional
-  public Profile getProfileById(long profileId) {
-    return profileRepository.findById(profileId)
-                            .orElseThrow(() -> new ApplicationException(ProfileErrorCode.NOTFOUND_PROFILE));
-  }
-
-  @Transactional
-  public Profile updateByUserId(long userId, ProfileUpdateRequest dto) {
-    User user = this.userService.getUserById(userId);
-    Profile profile = getProfileById(user.getProfile().getId());
-
-    ProfileBasic profileBasic = dto.toProfileBasic();
-    ProfileBio profileBio = dto.toProfileBio();
-
-    profile.updateBasic(profileBasic);
-    profile.updateBio(profileBio);
-
-    return profile;
-  }
-
-  @Transactional
-  public Profile updateProfileValues(long userId, ProfileValuePickUpdateRequest dto) {
-    User user = this.userService.getUserById(userId);
-    Profile profile = getProfileById(user.getProfile().getId());
-
-    List<ProfileValuePick> profileValuePicks = profile.getProfileValuePicks();
-
-    Map<Long, Integer> userValueItemAnswerMap = new HashMap<>();
-    for (ProfileValuePickPair profileValuePickPair : dto.profileValuePickPairs()) {
-      final Long valueItemId = profileValuePickPair.valueItemId();
-      final Integer selectedAnswer = profileValuePickPair.selectedAnswer();
-
-      userValueItemAnswerMap.put(valueItemId, selectedAnswer);
+        return savedProfile;
     }
 
-    for (ProfileValuePick profileValuePick : profileValuePicks) {
-      final Long profileValueItemID = profileValuePick.getValuePick().getId();
-      profileValuePick.updatedSelectedAnswer(userValueItemAnswerMap.get(profileValueItemID));
+    @Transactional
+    public Profile getProfileById(long profileId) {
+        return profileRepository.findById(profileId)
+            .orElseThrow(() -> new ApplicationException(ProfileErrorCode.NOTFOUND_PROFILE));
     }
 
-    return profile;
-  }
+    @Transactional
+    public Profile updateByUserId(long userId, ProfileUpdateRequest dto) {
+        User user = this.userService.getUserById(userId);
+        Profile profile = getProfileById(user.getProfile().getId());
 
-  public boolean isNicknameAvailable(String nickname) {
-    return !profileRepository.existsByProfileBasic_Nickname(nickname);
-  }
+        ProfileBasic profileBasic = dto.toProfileBasic();
+        ProfileBio profileBio = dto.toProfileBio();
+
+        profile.updateBasic(profileBasic);
+        profile.updateBio(profileBio);
+
+        return profile;
+    }
+
+    @Transactional
+    public Profile updateProfileValues(long userId, ProfileValuePickUpdateRequest dto) {
+        User user = this.userService.getUserById(userId);
+        Profile profile = getProfileById(user.getProfile().getId());
+
+        List<ProfileValuePick> profileValuePicks = profile.getProfileValuePicks();
+
+        Map<Long, Integer> userValueItemAnswerMap = new HashMap<>();
+        for (ProfileValuePickPair profileValuePickPair : dto.profileValuePickPairs()) {
+            final Long valueItemId = profileValuePickPair.valueItemId();
+            final Integer selectedAnswer = profileValuePickPair.selectedAnswer();
+
+            userValueItemAnswerMap.put(valueItemId, selectedAnswer);
+        }
+
+        for (ProfileValuePick profileValuePick : profileValuePicks) {
+            final Long profileValueItemID = profileValuePick.getValuePick().getId();
+            profileValuePick.updatedSelectedAnswer(userValueItemAnswerMap.get(profileValueItemID));
+        }
+
+        return profile;
+    }
+
+    public boolean isNicknameAvailable(String nickname) {
+        return !profileRepository.existsByProfileBasic_Nickname(nickname);
+    }
 }
