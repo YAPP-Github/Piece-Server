f7a05c23bc69a46faeb61a18b21096a05c8769e7
LujaeC <youjae0803@gmail.com>
2025-01-08 22:35:47 +0900

    [PC-297] fix: [ValueTalk] subtitle -> category

--- common/src/main/java/org/yapp/domain/value/ValueTalk.java
+++ common/src/main/java/org/yapp/domain/value/ValueTalk.java
@@ -1,6 +1,11 @@
 package org.yapp.domain.value;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,13 +19,14 @@ import org.hibernate.annotations.ColumnDefault;
 @AllArgsConstructor
 @Builder
 public class ValueTalk {
+
     @Id
     @Column(name = "value_talk_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
-    private String subTitle;
+    private String category;
 
     @Column(nullable = false)
     private String title;
