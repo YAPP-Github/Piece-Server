db9791f365e292c7144730bf26370cbfadcbd35f
LujaeC <youjae0803@gmail.com>
2025-01-08 22:48:21 +0900

    [PC-297] chore: 가치관 톡 태그 변경

--- api/src/main/java/org/yapp/domain/value/presentation/ValueTalkController.java
+++ api/src/main/java/org/yapp/domain/value/presentation/ValueTalkController.java
@@ -2,6 +2,7 @@ package org.yapp.domain.value.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +14,21 @@ import org.yapp.domain.value.application.ValueTalkService;
 import org.yapp.domain.value.presentation.dto.ValueTalkResponses;
 import org.yapp.util.CommonResponse;
 
-import java.util.List;
-
 @RequestMapping("/api/valueTalks")
 @RestController
 @RequiredArgsConstructor
 public class ValueTalkController {
+
     private final ValueTalkService valueTalkService;
 
     @GetMapping()
-    @Operation(summary = "가치관 톡 리스트 조회", description = "서비스에 등록된 모든 가치관 톡 질문을 조회합니다.", tags = {"ValueTalk"})
+    @Operation(summary = "가치관 톡 리스트 조회", description = "서비스에 등록된 모든 가치관 톡 질문을 조회합니다.", tags = {
+        "가치관 톡"})
     @ApiResponse(responseCode = "200", description = "가치관 톡 리스트가 성공적으로 조회되었습니다.")
     public ResponseEntity<CommonResponse<ValueTalkResponses>> getValueTalks() {
         List<ValueTalk> valueTalks = valueTalkService.getAllValueTalksActive();
         return ResponseEntity.status(HttpStatus.OK)
-                .body(CommonResponse.createSuccess(ValueTalkResponses.from(valueTalks)));
+            .body(CommonResponse.createSuccess(ValueTalkResponses.from(valueTalks)));
     }
 
 }
