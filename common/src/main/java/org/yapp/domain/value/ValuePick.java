f6bc7db0be4d9da8fec93e9ef4d168e07aebf98b
LujaeC <youjae0803@gmail.com>
2025-01-08 22:08:21 +0900

    [PC-297] feat: ValuePick isActive 필드 추가

--- common/src/main/java/org/yapp/domain/value/ValuePick.java
+++ common/src/main/java/org/yapp/domain/value/ValuePick.java
@@ -1,10 +1,5 @@
 package org.yapp.domain.value;
 
-import lombok.AllArgsConstructor;
-import org.hibernate.annotations.Type;
-
-import java.util.Map;
-
 import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,9 +7,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name = "value_pick")
@@ -23,18 +21,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ValuePick {
-  @Id
-  @Column(name = "value_pick_id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
 
-  @Column
-  private String category;
+    @Id
+    @Column(name = "value_pick_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String question;
+    @Column
+    private String category;
 
-  @Type(JsonType.class)
-  @Column(name = "answers", columnDefinition = "longtext", nullable = false)
-  private Map<Integer, Object> answers;
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    @Type(JsonType.class)
+    @Column(name = "answers", columnDefinition = "longtext", nullable = false)
+    private Map<Integer, Object> answers;
 }
