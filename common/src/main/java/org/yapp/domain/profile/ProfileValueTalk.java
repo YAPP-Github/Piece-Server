5c190da2f50fb1bf525ee7abb93437afd54a9535
LujaeC <youjae0803@gmail.com>
2025-01-08 22:37:39 +0900

    [PC-297] fix: [ProfileValueTalk] ValueTalk 조인 컬럼 수정

--- common/src/main/java/org/yapp/domain/profile/ProfileValueTalk.java
+++ common/src/main/java/org/yapp/domain/profile/ProfileValueTalk.java
@@ -1,6 +1,14 @@
 package org.yapp.domain.profile;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +22,7 @@ import org.yapp.domain.value.ValueTalk;
 @AllArgsConstructor
 @Builder
 public class ProfileValueTalk {
+
     @Id
     @Column(name = "profile_value_talk_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +33,7 @@ public class ProfileValueTalk {
     private Profile profile;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "value_item_id", nullable = false)
+    @JoinColumn(name = "value_talk_id", nullable = false)
     private ValueTalk valueTalk;
 
     @Column
