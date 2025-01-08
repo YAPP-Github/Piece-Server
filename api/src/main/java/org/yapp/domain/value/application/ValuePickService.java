fff525a917e22ba5ae0655bcb87d73d9513bf4cc
LujaeC <youjae0803@gmail.com>
2025-01-08 22:19:19 +0900

    [PC-297] fix: 활성화된 가치관 픽 조회

--- api/src/main/java/org/yapp/domain/value/application/ValuePickService.java
+++ api/src/main/java/org/yapp/domain/value/application/ValuePickService.java
@@ -1,19 +1,18 @@
 package org.yapp.domain.value.application;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.yapp.domain.value.ValuePick;
 import org.yapp.domain.value.dao.ValuePickRepository;
 
-import java.util.List;
-
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class ValuePickService {
-  private final ValuePickRepository valuePickRepository;
 
-  public List<ValuePick> getAllValueItems() {
-    return valuePickRepository.findAll();
-  }
+    private final ValuePickRepository valuePickRepository;
+
+    public List<ValuePick> getAllActiveValuePicks() {
+        return valuePickRepository.findAllByIsActiveTrue();
+    }
 }
--- api/src/main/java/org/yapp/domain/value/dao/ValuePickRepository.java
+++ api/src/main/java/org/yapp/domain/value/dao/ValuePickRepository.java
@@ -1,9 +1,12 @@
 package org.yapp.domain.value.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yapp.domain.value.ValuePick;
 
 @Repository
 public interface ValuePickRepository extends JpaRepository<ValuePick, Long> {
+
+    List<ValuePick> findAllByIsActiveTrue();
 }
