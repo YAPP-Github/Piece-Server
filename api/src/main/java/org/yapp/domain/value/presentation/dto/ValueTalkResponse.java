c6b7c4817a337e4a19b441ed4573e32b01e1874c
LujaeC <youjae0803@gmail.com>
2025-01-09 22:20:22 +0900

    [PC-297] fix: 가치관 Talk 응답 필드 이름 수정

--- api/src/main/java/org/yapp/domain/value/presentation/dto/ValueTalkResponse.java
+++ api/src/main/java/org/yapp/domain/value/presentation/dto/ValueTalkResponse.java
@@ -2,7 +2,7 @@ package org.yapp.domain.value.presentation.dto;
 
 import org.yapp.domain.value.ValueTalk;
 
-public record ValueTalkResponse(Long id, String subTitle, String title) {
+public record ValueTalkResponse(Long id, String category, String title) {
 
     public static ValueTalkResponse from(ValueTalk valueTalk) {
         return new ValueTalkResponse(valueTalk.getId(), valueTalk.getCategory(),
