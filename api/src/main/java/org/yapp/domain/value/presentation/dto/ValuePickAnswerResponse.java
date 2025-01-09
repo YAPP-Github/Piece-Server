88e3b44ac4b6934d42f67ed62d004cfe0c18924f
LujaeC <youjae0803@gmail.com>
2025-01-09 22:19:43 +0900

    [PC-297] fix: 가치관 Pick 조회 응답 형식 변경

---
+++ api/src/main/java/org/yapp/domain/value/presentation/dto/ValuePickAnswerResponse.java
@@ -0,0 +1,5 @@
+package org.yapp.domain.value.presentation.dto;
+
+public record ValuePickAnswerResponse(Integer number, String content) {
+
+}
--- api/src/main/java/org/yapp/domain/value/presentation/dto/ValuePickResponse.java
+++ api/src/main/java/org/yapp/domain/value/presentation/dto/ValuePickResponse.java
@@ -1,12 +1,20 @@
 package org.yapp.domain.value.presentation.dto;
 
+import java.util.List;
 import org.yapp.domain.value.ValuePick;
 
-import java.util.Map;
+public record ValuePickResponse(Long id, String category, String question,
+                                List<ValuePickAnswerResponse> answers) {
 
-public record ValuePickResponse(Long id, String category, String question, Map<Integer, Object> answers) {
-  public static ValuePickResponse from(ValuePick valuePick) {
-    return new ValuePickResponse(valuePick.getId(), valuePick.getCategory(), valuePick.getQuestion(),
-        valuePick.getAnswers());
-  }
+    public static ValuePickResponse from(ValuePick valuePick) {
+
+        List<ValuePickAnswerResponse> answerList = valuePick.getAnswers().entrySet()
+            .stream()
+            .map(entry -> new ValuePickAnswerResponse(entry.getKey(), (String) entry.getValue()))
+            .toList();
+
+        return new ValuePickResponse(valuePick.getId(), valuePick.getCategory(),
+            valuePick.getQuestion(),
+            answerList);
+    }
 }
