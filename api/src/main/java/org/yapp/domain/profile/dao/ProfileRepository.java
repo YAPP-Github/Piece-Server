03f5eabf9b6ba48e23f6668775be548e26312184
Chanho lee <devchlee12@gmail.com>
2025-01-11 23:56:39 +0900

    [PC-267] feat: 지역 기반 프로필 조회 쿼리 메서드 추가

--- api/src/main/java/org/yapp/domain/profile/dao/ProfileRepository.java
+++ api/src/main/java/org/yapp/domain/profile/dao/ProfileRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yapp.domain.profile.Profile;
 
+import java.util.List;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-    boolean existsByProfileBasic_Nickname(String nickname);
+  boolean existsByProfileBasic_Nickname(String nickname);
+
+  List<Profile> findByProfileBasic_Location(String location);
 }
