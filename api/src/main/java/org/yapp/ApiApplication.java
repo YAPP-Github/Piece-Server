899b5968e96a10692067df615e89ba79f1acd177
Chanho lee <devchlee12@gmail.com>
2025-01-11 23:48:47 +0900

    [PC-267] feat: 스케줄링 허용 설정

--- api/src/main/java/org/yapp/ApiApplication.java
+++ api/src/main/java/org/yapp/ApiApplication.java
@@ -1,25 +1,28 @@
 package org.yapp;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
+@EnableScheduling
 @SpringBootApplication
 public class ApiApplication {
-    private final TestBean testBean;
+  private final TestBean testBean;
 
-    @Autowired
-    public ApiApplication(TestBean testBean) {
-        this.testBean = testBean;
-    }
+  @Autowired
+  public ApiApplication(TestBean testBean) {
+    this.testBean = testBean;
+  }
 
-    @PostConstruct
-    public void dependencyTest() {
-        testBean.dependencyTest();
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ApiApplication.class, args);
+  }
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
-    }
+  @PostConstruct
+  public void dependencyTest() {
+    testBean.dependencyTest();
+  }
 }
