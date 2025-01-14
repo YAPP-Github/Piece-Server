8b44976630357aa1f4ec962a9814ad9e8d7f7220
LujaeC <youjae0803@gmail.com>
2025-01-14 18:48:06 +0900

    [PC-361] feat: ProfileValueTalk 저장소 profileID 조회 기능 추가

--- api/src/main/java/org/yapp/domain/profile/dao/ProfileValueTalkRepository.java
+++ api/src/main/java/org/yapp/domain/profile/dao/ProfileValueTalkRepository.java
@@ -1,10 +1,13 @@
 package org.yapp.domain.profile.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yapp.domain.profile.ProfileValueTalk;
 
 @Repository
 public interface ProfileValueTalkRepository extends JpaRepository<ProfileValueTalk, Long> {
+
+    List<ProfileValueTalk> findByProfileId(Long profileId);
 
 }
