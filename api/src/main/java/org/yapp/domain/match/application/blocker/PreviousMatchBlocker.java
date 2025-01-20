2bedc633c02b6ccbf3ff12563b2aeb4f9aab183b
Chanho Lee <devchlee12@gmail.com>
2025-01-21 01:28:30 +0900

    [PC-268] feat: 이전 매칭 기반 차단기 구현

--- api/src/main/java/org/yapp/domain/match/application/blocker/PreviousMatchBlocker.java
+++ api/src/main/java/org/yapp/domain/match/application/blocker/PreviousMatchBlocker.java
@@ -1,8 +1,17 @@
 package org.yapp.domain.match.application.blocker;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.yapp.domain.match.application.MatchService;
+
+@Component
+@RequiredArgsConstructor
 public class PreviousMatchBlocker implements Blocker {
+
+  private final MatchService matchService;
+
   @Override
   public boolean blocked(Long blockingUserId, Long blockedUserId) {
-    return false;
+    return matchService.wasUsersMatched(blockingUserId, blockedUserId);
   }
 }
