760f59ef0b6a36ae82ab56cfaa6e18873fe07c62
Chanho Lee <devchlee12@gmail.com>
2025-01-21 01:26:11 +0900

    [PC-268] fix: 매칭 내역 저장, 유저 매치 확인 메서드 추가

--- api/src/main/java/org/yapp/domain/match/application/MatchService.java
+++ api/src/main/java/org/yapp/domain/match/application/MatchService.java
@@ -1,23 +1,32 @@
 package org.yapp.domain.match.application;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.yapp.application.AuthenticationService;
 import org.yapp.domain.match.MatchInfo;
 import org.yapp.domain.match.dao.MatchInfoRepository;
+import org.yapp.domain.user.User;
+import org.yapp.domain.user.application.UserService;
 import org.yapp.error.dto.MatchErrorCode;
 import org.yapp.error.exception.ApplicationException;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MatchService {
+
   private final MatchInfoRepository matchInfoRepository;
   private final AuthenticationService authenticationService;
+  private final UserService userService;
+
+  public MatchInfo createMatchInfo(Long user1Id, Long user2Id) {
+    User user1 = userService.getUserById(user1Id);
+    User user2 = userService.getUserById(user2Id);
+    return matchInfoRepository.save(new MatchInfo(LocalDate.now(), user1, user2));
+  }
 
   public LocalDate getMatchDate() {
     LocalDateTime nowDateTime = LocalDateTime.now();
@@ -30,9 +39,14 @@ public class MatchService {
     return nowDate;
   }
 
+  public boolean wasUsersMatched(Long user1Id, Long user2Id) {
+    Optional<MatchInfo> matchInfoByIds = matchInfoRepository.findMatchInfoByIds(user1Id, user2Id);
+    return matchInfoByIds.isPresent();
+  }
+
   public MatchInfo getMatchInfo() {
     Long userId = authenticationService.getUserId();
     return matchInfoRepository.findByUserIdAndDate(userId, getMatchDate())
-                              .orElseThrow(() -> new ApplicationException(MatchErrorCode.NOTFOUND_MATCH));
+        .orElseThrow(() -> new ApplicationException(MatchErrorCode.NOTFOUND_MATCH));
   }
 }
