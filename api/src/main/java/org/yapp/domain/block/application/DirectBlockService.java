fde7c232eeb88522414e7f997d14c5c8e0ea999e
Chanho Lee <devchlee12@gmail.com>
2025-01-21 23:21:18 +0900

    [PC-273] feat: 유저 직접 차단 및 차단 확인 기능 구현

---
+++ api/src/main/java/org/yapp/domain/block/application/DirectBlockService.java
@@ -0,0 +1,26 @@
+package org.yapp.domain.block.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.yapp.application.AuthenticationService;
+import org.yapp.domain.block.DirectBlock;
+import org.yapp.domain.block.dao.DirectBlockRepository;
+
+@Service
+@RequiredArgsConstructor
+public class DirectBlockService {
+
+  private final DirectBlockRepository directBlockRepository;
+  private final AuthenticationService authenticationService;
+
+  public DirectBlock blockUser(Long blockId) {
+    Long userId = authenticationService.getUserId();
+    return directBlockRepository.save(new DirectBlock(userId, blockId));
+  }
+
+  public boolean checkBlock(Long userId, Long partnerId) {
+    return directBlockRepository.existsDirectBlockByBlockingUserIdAndBlockedUserId(userId,
+        partnerId);
+  }
+
+}
