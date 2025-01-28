8c215371f68d7b5b03dd9c53c3d11f235b945636
Chanho Lee <devchlee12@gmail.com>
2025-01-28 20:13:25 +0900

    [PC-272] feat: 이미 신고된 유저 조회 쿼리 추가

--- api/src/main/java/org/yapp/domain/report/dao/ReportRepository.java
+++ api/src/main/java/org/yapp/domain/report/dao/ReportRepository.java
@@ -2,6 +2,9 @@ package org.yapp.domain.report.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.yapp.domain.report.Report;
+import org.yapp.domain.user.User;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+  boolean existsReportByReporterAndReportedUser(User reporter, User reportedUser);
 }
