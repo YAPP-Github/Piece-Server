c1433bea7541c20e33deb26a76f57ade419fc153
LujaeC <youjae0803@gmail.com>
2025-01-30 12:03:35 +0900

    [PC-425] refactor: testBean 삭제

--- api/src/main/java/org/yapp/ApiApplication.java
+++ api/src/main/java/org/yapp/ApiApplication.java
@@ -1,28 +1,14 @@
 package org.yapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import jakarta.annotation.PostConstruct;
-
 @EnableScheduling
 @SpringBootApplication
 public class ApiApplication {
-  private final TestBean testBean;
 
-  @Autowired
-  public ApiApplication(TestBean testBean) {
-    this.testBean = testBean;
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(ApiApplication.class, args);
-  }
-
-  @PostConstruct
-  public void dependencyTest() {
-    testBean.dependencyTest();
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
