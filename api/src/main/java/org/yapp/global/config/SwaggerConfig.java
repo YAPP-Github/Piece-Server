06bd0bbb3a7c2479262cab0115ffd07b24247f19
LujaeC <youjae0803@gmail.com>
2025-01-30 11:43:56 +0900

    [PC-425] refactor: SwaggerConfig 파일 모듈 위치 변경 (common -> api)

--- api/src/main/java/org/yapp/global/config/SwaggerConfig.java
+++ api/src/main/java/org/yapp/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.yapp.config;
+package org.yapp.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -10,27 +10,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
         String jwt = "JWT";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
         Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
+            .name(jwt)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
         );
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo())
-                .addSecurityItem(securityRequirement)
-                .components(components);
+            .components(new Components())
+            .info(apiInfo())
+            .addSecurityItem(securityRequirement)
+            .components(components);
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("API Documentation") // API의 제목
-                .description("Admin, API Description") // API에 대한 설명
-                .version("1.0.0"); // API의 버전
+            .title("API Documentation") // API의 제목
+            .description("Admin, API Description") // API에 대한 설명
+            .version("1.0.0"); // API의 버전
     }
 }
