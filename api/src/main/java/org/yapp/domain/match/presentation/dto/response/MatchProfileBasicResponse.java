1c596518392c38260e6817b0cab81e1e26584db1
Chanho Lee <devchlee12@gmail.com>
2025-01-31 23:50:40 +0900

    [PC-443] fix: MatchProfileBasicResponse 누락된 변수들 추가

--- api/src/main/java/org/yapp/domain/match/presentation/dto/response/MatchProfileBasicResponse.java
+++ api/src/main/java/org/yapp/domain/match/presentation/dto/response/MatchProfileBasicResponse.java
@@ -15,20 +15,27 @@ public class MatchProfileBasicResponse {
   private Long matchId;
   private String description;
   private String nickname;
-  private String age;
+  private Integer age;
   private String birthYear;
+  private Integer height;
+  private Integer weight;
   private String location;
   private String job;
+  private String smokingStatus;
 
   public static MatchProfileBasicResponse fromProfile(Long matchId, Profile profile) {
     String nickname = profile.getProfileBasic().getNickname();
     LocalDate birthDate = profile.getProfileBasic().getBirthdate();
     LocalDate now = LocalDate.now();
-    String age = String.valueOf(Period.between(birthDate, now).getYears());
+    Integer age = Period.between(birthDate, now).getYears();
     String birthYearFormatted = String.valueOf(birthDate.getYear()).substring(2);
     String location = profile.getProfileBasic().getLocation();
+    String description = profile.getProfileBasic().getDescription();
+    Integer height = profile.getProfileBasic().getHeight();
+    Integer weight = profile.getProfileBasic().getWeight();
     String job = profile.getProfileBasic().getJob();
-    return new MatchProfileBasicResponse(matchId, "", nickname, birthYearFormatted, age, location,
-        job);
+    String smokingStatus = profile.getProfileBasic().getSmokingStatus();
+    return new MatchProfileBasicResponse(matchId, description, nickname, age, birthYearFormatted
+        , height, weight, location, job, smokingStatus);
   }
 }
