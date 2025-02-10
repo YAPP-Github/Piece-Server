9215b8a6fdf3bd309f465e0a946831394d4429f0
Chanho Lee <devchlee12@gmail.com>
2025-02-11 00:19:23 +0900

    [PC-447] feat: Setting 엔티티 추가

---
+++ core/domain/src/main/java/org/yapp/core/domain/setting/Setting.java
@@ -0,0 +1,51 @@
+package org.yapp.core.domain.setting;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Setting {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "user_id")
+  private Long userId;
+
+  @Column(name = "match_notification")
+  private boolean matchNotification = true;
+
+  @Column(name = "notification")
+  private boolean notification = true;
+
+  @Column(name = "acquaintance_block")
+  private boolean acquaintanceBlock = true;
+
+  public Setting(Long userId, boolean matchNotification, boolean notification,
+      boolean acquaintanceBlock) {
+    this.userId = userId;
+    this.matchNotification = matchNotification;
+    this.notification = notification;
+    this.acquaintanceBlock = acquaintanceBlock;
+  }
+
+  public void updateMatchNotification(boolean status) {
+    this.matchNotification = status;
+  }
+
+  public void updateNotification(boolean status) {
+    this.notification = status;
+  }
+
+  public void updateAcquaintanceBlock(boolean status) {
+    this.acquaintanceBlock = status;
+  }
+}
