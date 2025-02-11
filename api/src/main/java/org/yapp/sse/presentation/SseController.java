4648b673d9ea249957ca48a4e7ad5556976d0435
LujaeC <youjae0803@gmail.com>
2025-02-12 01:26:56 +0900

    [PC-545] fix: SSE 연결 HTTP 메서드 Post로 수정

--- api/src/main/java/org/yapp/sse/presentation/SseController.java
+++ api/src/main/java/org/yapp/sse/presentation/SseController.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yapp.core.sse.FluxSsePersonalService;
@@ -19,7 +19,7 @@ public class SseController {
     private final FluxSsePersonalService ssePersonalService;
 
     @PreAuthorize(value = "hasRole('USER')")
-    @GetMapping("/personal")
+    @PostMapping("/personal/connect")
     @Operation(summary = "SSE 연결", description = "로그인 회원에 대한 이벤트 수신 스트림을 얻습니다", tags = {"SSE"})
     public Flux<ServerSentEvent<Object>> streamEvents(@AuthenticationPrincipal Long userId) {
         return ssePersonalService.connect(userId);
