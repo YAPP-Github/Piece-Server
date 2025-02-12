1d3d591df1948f6cd94d97e276026f3acd222444
Chanho Lee <devchlee12@gmail.com>
2025-02-12 23:49:54 +0900

    [PC-579] feat: 매칭 거절을 위한 필드 추가

--- core/domain/src/main/java/org/yapp/core/domain/match/MatchInfo.java
+++ core/domain/src/main/java/org/yapp/core/domain/match/MatchInfo.java
@@ -17,56 +17,62 @@ import org.yapp.core.domain.user.User;
 @NoArgsConstructor
 public class MatchInfo {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(name = "date")
-    private LocalDate date;
+  @Column(name = "date")
+  private LocalDate date;
 
-    @ManyToOne
-    @JoinColumn(name = "user_1")
-    private User user1;
+  @ManyToOne
+  @JoinColumn(name = "user_1")
+  private User user1;
 
-    @Column(name = "user_1_piece_checked")
-    private Boolean user1PieceChecked = false;
+  @Column(name = "user_1_piece_checked")
+  private Boolean user1PieceChecked = false;
 
-    @Column(name = "user_1_accept")
-    private Boolean user1Accepted = false;
+  @Column(name = "user_1_refused")
+  private Boolean user1Refused = false;
 
-    @ManyToOne
-    @JoinColumn(name = "user_2")
-    private User user2;
+  @Column(name = "user_1_accept")
+  private Boolean user1Accepted = false;
 
-    @Column(name = "user_2_piece_checked")
-    private Boolean user2PieceChecked = false;
+  @ManyToOne
+  @JoinColumn(name = "user_2")
+  private User user2;
 
-    @Column(name = "user_2_accept")
-    private Boolean user2Accepted = false;
+  @Column(name = "user_2_piece_checked")
+  private Boolean user2PieceChecked = false;
 
-    public MatchInfo(LocalDate date, User user1, User user2) {
-        this.date = date;
-        this.user1 = user1;
-        this.user2 = user2;
+  @Column(name = "user_2_refused")
+  private Boolean user2Refused = false;
+
+  @Column(name = "user_2_accept")
+  private Boolean user2Accepted = false;
+
+  public MatchInfo(LocalDate date, User user1, User user2) {
+    this.date = date;
+    this.user1 = user1;
+    this.user2 = user2;
+  }
+
+  public static MatchInfo createMatchInfo(User user1, User user2) {
+    return new MatchInfo(LocalDate.now(), user1, user2);
+  }
+
+  public void checkPiece(Long userId) {
+    if (user1.getId().equals(userId)) {
+      user1PieceChecked = true;
+    } else {
+      user2PieceChecked = true;
     }
+  }
 
-    public static MatchInfo createMatchInfo(User user1, User user2) {
-        return new MatchInfo(LocalDate.now(), user1, user2);
+  public void acceptPiece(Long userId) {
+    if (user1.getId().equals(userId)) {
+      user1Accepted = true;
+    } else {
+      user2Accepted = true;
     }
-
-    public void checkPiece(Long userId) {
-        if (user1.getId().equals(userId)) {
-            user1PieceChecked = true;
-        } else {
-            user2PieceChecked = true;
-        }
-    }
-
-    public void acceptPiece(Long userId) {
-        if (user1.getId().equals(userId)) {
-            user1Accepted = true;
-        } else {
-            user2Accepted = true;
-        }
-    }
+  }
 }
