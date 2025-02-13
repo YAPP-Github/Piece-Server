848767cdb6c7795b268b7d4942d36e85ac9be889
LujaeC <youjae0803@gmail.com>
2025-02-14 07:34:26 +0900

    [PC-175] fix: OauthLoginRequest 필드 이름 수정 (token -> oauthCredential)

--- api/src/main/java/org/yapp/domain/auth/application/oauth/service/OauthService.java
+++ api/src/main/java/org/yapp/domain/auth/application/oauth/service/OauthService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.yapp.core.auth.AuthToken;
 import org.yapp.core.auth.AuthTokenGenerator;
-import org.yapp.core.auth.jwt.JwtUtil;
 import org.yapp.core.auth.token.RefreshTokenService;
 import org.yapp.core.domain.user.RoleStatus;
 import org.yapp.core.domain.user.User;
@@ -19,70 +18,73 @@ import org.yapp.domain.user.dao.UserRepository;
 @RequiredArgsConstructor
 public class OauthService {
 
-  private final OauthProviderResolver oauthProviderResolver;
-  private final JwtUtil jwtUtil;
-  private final UserRepository userRepository;
-  private final RefreshTokenService refreshTokenService;
-  private final AuthTokenGenerator authTokenGenerator;
+    private final OauthProviderResolver oauthProviderResolver;
+    private final UserRepository userRepository;
+    private final RefreshTokenService refreshTokenService;
+    private final AuthTokenGenerator authTokenGenerator;
 
-  public OauthLoginResponse login(OauthLoginRequest request) {
-    OauthProvider oauthProvider = oauthProviderResolver.find(request.getProviderName());
-    String oauthId =
-        request.getProviderName() + oauthProvider.getOAuthProviderUserId(request.getToken());
+    public OauthLoginResponse login(OauthLoginRequest request) {
+        OauthProvider oauthProvider = oauthProviderResolver.find(request.getProviderName());
+        String oauthId =
+            request.getProviderName() + oauthProvider.getOAuthProviderUserId(
+                request.getOauthCredential());
 
-    //이미 가입된 유저인지 확인하고 가입되어 있지 않으면 회원가입 처리
-    Optional<User> userOptional = userRepository.findByOauthId(oauthId);
-    if (userOptional.isEmpty()) {
-      User newUser = User.builder().oauthId(oauthId).role(RoleStatus.NONE.getStatus()).build();
-      User savedUser = userRepository.save(newUser);
-      Long userId = savedUser.getId();
-      AuthToken token = authTokenGenerator.generate(userId, savedUser.getOauthId(), "NONE");
-      String accessToken = token.accessToken();
-      String refreshToken = token.refreshToken();
-      refreshTokenService.saveRefreshToken(userId, refreshToken);
-      return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
+        //이미 가입된 유저인지 확인하고 가입되어 있지 않으면 회원가입 처리
+        Optional<User> userOptional = userRepository.findByOauthId(oauthId);
+        if (userOptional.isEmpty()) {
+            User newUser = User.builder().oauthId(oauthId).role(RoleStatus.NONE.getStatus())
+                .build();
+            User savedUser = userRepository.save(newUser);
+            Long userId = savedUser.getId();
+            AuthToken token = authTokenGenerator.generate(userId, savedUser.getOauthId(), "NONE");
+            String accessToken = token.accessToken();
+            String refreshToken = token.refreshToken();
+            refreshTokenService.saveRefreshToken(userId, refreshToken);
+            return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
+        }
+
+        //이미 가입한 유저인 경우 로그인 처리
+        Long userId = userOptional.get().getId();
+        final User user = userOptional.get();
+
+        AuthToken token = authTokenGenerator.generate(userId, user.getOauthId(), user.getRole());
+        String accessToken = token.accessToken();
+        String refreshToken = token.refreshToken();
+        refreshTokenService.saveRefreshToken(userId, refreshToken);
+
+        //아직 SMS 인증이 완료되지 않음
+        if (user.getRole().equals("NONE")) {
+            return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
+        }
+
+        // SMS 인증은 완료되었으나, 프로필 등록을 안함
+        if (user.getRole().equals("REGISTER")) {
+            return new OauthLoginResponse(RoleStatus.REGISTER.getStatus(), accessToken,
+                refreshToken);
+        }
+
+        //프로필 등록은 했지만, 심사중
+        if (user.getRole().equals("PENDING")) {
+            return new OauthLoginResponse(RoleStatus.PENDING.getStatus(), accessToken,
+                refreshToken);
+        }
+
+        // 가입과 프로필 등록까지 완료된 유저
+        return new OauthLoginResponse(user.getRole(), accessToken, refreshToken);
     }
 
-    //이미 가입한 유저인 경우 로그인 처리
-    Long userId = userOptional.get().getId();
-    final User user = userOptional.get();
 
-    AuthToken token = authTokenGenerator.generate(userId, user.getOauthId(), user.getRole());
-    String accessToken = token.accessToken();
-    String refreshToken = token.refreshToken();
-    refreshTokenService.saveRefreshToken(userId, refreshToken);
+    public OauthLoginResponse tmpTokenGet(Long userId) {
+        //이미 가입된 유저인지 확인하고 가입되어 있지 않으면 회원가입 처리
+        Optional<User> userOptional = userRepository.findById(userId);
 
-    //아직 SMS 인증이 완료되지 않음
-    if (user.getRole().equals("NONE")) {
-      return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
+        User user = userOptional.get();
+
+        AuthToken token = authTokenGenerator.generate(userId, user.getOauthId(), user.getRole());
+        String accessToken = token.accessToken();
+        String refreshToken = token.refreshToken();
+        refreshTokenService.saveRefreshToken(userId, refreshToken);
+
+        return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
     }
-
-    // SMS 인증은 완료되었으나, 프로필 등록을 안함
-    if (user.getRole().equals("REGISTER")) {
-      return new OauthLoginResponse(RoleStatus.REGISTER.getStatus(), accessToken, refreshToken);
-    }
-
-    //프로필 등록은 했지만, 심사중
-    if (user.getRole().equals("PENDING")) {
-      return new OauthLoginResponse(RoleStatus.PENDING.getStatus(), accessToken, refreshToken);
-    }
-
-    // 가입과 프로필 등록까지 완료된 유저
-    return new OauthLoginResponse(user.getRole(), accessToken, refreshToken);
-  }
-
-
-  public OauthLoginResponse tmpTokenGet(Long userId) {
-    //이미 가입된 유저인지 확인하고 가입되어 있지 않으면 회원가입 처리
-    Optional<User> userOptional = userRepository.findById(userId);
-
-    User user = userOptional.get();
-
-    AuthToken token = authTokenGenerator.generate(userId, user.getOauthId(), user.getRole());
-    String accessToken = token.accessToken();
-    String refreshToken = token.refreshToken();
-    refreshTokenService.saveRefreshToken(userId, refreshToken);
-
-    return new OauthLoginResponse(RoleStatus.NONE.getStatus(), accessToken, refreshToken);
-  }
 }
