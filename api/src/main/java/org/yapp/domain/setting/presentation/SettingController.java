64302822a04d5abd76a5601908758a9e73d9c10b
Chanho Lee <devchlee12@gmail.com>
2025-02-13 23:43:54 +0900

    [PC-598] feat: 연락처 동기화 시간 가져오기 API 엔드포인트 추가

--- api/src/main/java/org/yapp/domain/setting/presentation/SettingController.java
+++ api/src/main/java/org/yapp/domain/setting/presentation/SettingController.java
@@ -1,5 +1,7 @@
 package org.yapp.domain.setting.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yapp.domain.setting.application.BlockContactSyncTimeService;
 import org.yapp.domain.setting.application.SettingService;
 import org.yapp.domain.setting.dto.request.SettingToggleRequest;
+import org.yapp.domain.setting.dto.response.BlockContactSyncTimeResponse;
 import org.yapp.domain.setting.dto.response.SettingInfoResponse;
 import org.yapp.format.CommonResponse;
 
@@ -19,6 +23,7 @@ import org.yapp.format.CommonResponse;
 public class SettingController {
 
   private final SettingService settingService;
+  private final BlockContactSyncTimeService blockContactSyncTimeService;
 
   @GetMapping("/infos")
   public ResponseEntity<CommonResponse<SettingInfoResponse>> getInfos(
@@ -52,5 +57,15 @@ public class SettingController {
     return ResponseEntity.ok(CommonResponse.createSuccessWithNoContent());
   }
 
-
+  @GetMapping("/blocks/contacts/sync-time")
+  @Operation(summary = "연락처 차단 동기화 시간 조회", description = "연락처 차단 마지막 동기화 시간을 조회합니다.", tags = {
+      "설정"})
+  @ApiResponse(responseCode = "200", description = "연락처 차단 동기화 시간이 성공적으로 조회되었습니다.")
+  public ResponseEntity<CommonResponse<BlockContactSyncTimeResponse>> getBlockContactSyncTime(
+      @AuthenticationPrincipal Long userId
+  ) {
+    BlockContactSyncTimeResponse blockContactSyncTimeResponse = blockContactSyncTimeService.getBlockContactSyncTimeResponse(
+        userId);
+    return ResponseEntity.ok(CommonResponse.createSuccess(blockContactSyncTimeResponse));
+  }
 }
