b03662b6d32feb900e7a8fa1544ab0362abe1dc0
Chanho Lee <devchlee12@gmail.com>
2025-02-15 17:18:50 +0900

    [PC-607] feat: 회원 삭제 로직 추가

--- api/src/main/java/org/yapp/domain/user/application/UserService.java
+++ api/src/main/java/org/yapp/domain/user/application/UserService.java
@@ -19,75 +19,80 @@ import org.yapp.domain.user.presentation.response.UserRejectHistoryResponse;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final UserRejectHistoryRepository userRejectHistoryRepository;
-    private final JwtUtil jwtUtil;
+  private final UserRepository userRepository;
+  private final UserRejectHistoryRepository userRejectHistoryRepository;
+  private final JwtUtil jwtUtil;
 
-    /**
-     * Role을 USER로 바꾸고 변경된 토큰을 반환한다.
-     *
-     * @return 액세스토큰과 리프레시 토큰
-     */
-    @Transactional
-    public OauthLoginResponse completeProfileInitialize(Long userId, Profile profile) {
-        User user =
-            userRepository.findById(userId)
-                .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
-        user.setProfile(profile);
-        user.updateUserRole(RoleStatus.PENDING.getStatus());
-        String oauthId = user.getOauthId();
-        String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, user.getRole(),
-            600000L);
-        String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, user.getRole(),
-            864000000L);
-        return new OauthLoginResponse(RoleStatus.PENDING.getStatus(), accessToken, refreshToken);
-    }
-
-    public User getUserById(Long userId) {
-        return userRepository.findById(userId)
+  /**
+   * Role을 USER로 바꾸고 변경된 토큰을 반환한다.
+   *
+   * @return 액세스토큰과 리프레시 토큰
+   */
+  @Transactional
+  public OauthLoginResponse completeProfileInitialize(Long userId, Profile profile) {
+    User user =
+        userRepository.findById(userId)
             .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
+    user.setProfile(profile);
+    user.updateUserRole(RoleStatus.PENDING.getStatus());
+    String oauthId = user.getOauthId();
+    String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, user.getRole(),
+        600000L);
+    String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, user.getRole(),
+        864000000L);
+    return new OauthLoginResponse(RoleStatus.PENDING.getStatus(), accessToken, refreshToken);
+  }
+
+  public User getUserById(Long userId) {
+    return userRepository.findById(userId)
+        .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
+  }
+
+  /**
+   * Role을 Register로 바꾸고 변경된 토큰을 반환한다.
+   *
+   * @return 액세스토큰과 리프레시 토큰
+   */
+  @Transactional
+  public OauthLoginResponse registerPhoneNumber(Long userId, String phoneNumber) {
+    User user =
+        userRepository.findById(userId)
+            .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
+    user.updateUserRole(RoleStatus.REGISTER.getStatus());
+    user.initializePhoneNumber(phoneNumber);
+    String oauthId = user.getOauthId();
+    String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, user.getRole(),
+        600000L);
+    String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, user.getRole(),
+        864000000L);
+    return new OauthLoginResponse(RoleStatus.REGISTER.getStatus(), accessToken, refreshToken);
+  }
+
+  @Transactional(readOnly = true)
+  public UserRejectHistoryResponse getUserRejectHistoryLatest(Long userId) {
+    User user = this.getUserById(userId);
+    Profile profile = user.getProfile();
+
+    boolean reasonImage = false;
+    boolean reasonDescription = false;
+
+    UserRejectHistory userRejectHistory = userRejectHistoryRepository.findTopByUserIdOrderByCreatedAtDesc(
+        userId).orElse(null);
+
+    if (userRejectHistory != null) {
+      reasonImage = userRejectHistory.isReasonImage();
+      reasonDescription = userRejectHistory.isReasonDescription();
     }
 
-    /**
-     * Role을 Register로 바꾸고 변경된 토큰을 반환한다.
-     *
-     * @return 액세스토큰과 리프레시 토큰
-     */
-    @Transactional
-    public OauthLoginResponse registerPhoneNumber(Long userId, String phoneNumber) {
-        User user =
-            userRepository.findById(userId)
-                .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
-        user.updateUserRole(RoleStatus.REGISTER.getStatus());
-        user.initializePhoneNumber(phoneNumber);
-        String oauthId = user.getOauthId();
-        String accessToken = jwtUtil.createJwt("access_token", userId, oauthId, user.getRole(),
-            600000L);
-        String refreshToken = jwtUtil.createJwt("refresh_token", userId, oauthId, user.getRole(),
-            864000000L);
-        return new OauthLoginResponse(RoleStatus.REGISTER.getStatus(), accessToken, refreshToken);
-    }
+    return new UserRejectHistoryResponse(
+        profile.getProfileStatus(),
+        reasonImage,
+        reasonDescription
+    );
+  }
 
-    @Transactional(readOnly = true)
-    public UserRejectHistoryResponse getUserRejectHistoryLatest(Long userId) {
-        User user = this.getUserById(userId);
-        Profile profile = user.getProfile();
-
-        boolean reasonImage = false;
-        boolean reasonDescription = false;
-
-        UserRejectHistory userRejectHistory = userRejectHistoryRepository.findTopByUserIdOrderByCreatedAtDesc(
-            userId).orElse(null);
-
-        if (userRejectHistory != null) {
-            reasonImage = userRejectHistory.isReasonImage();
-            reasonDescription = userRejectHistory.isReasonDescription();
-        }
-
-        return new UserRejectHistoryResponse(
-            profile.getProfileStatus(),
-            reasonImage,
-            reasonDescription
-        );
-    }
+  @Transactional
+  public void deleteUser(Long userId) {
+    userRepository.deleteById(userId);
+  }
 }
