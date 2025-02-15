bf8f23b357bf03fc8200f668ef4da39fd64cb66c
Chanho Lee <devchlee12@gmail.com>
2025-02-16 00:31:39 +0900

    [PC-615] feat: 검증 추가

--- api/src/main/java/org/yapp/domain/user/presentation/dto/request/UserDeleteRequest.java
+++ api/src/main/java/org/yapp/domain/user/presentation/dto/request/UserDeleteRequest.java
@@ -1,5 +1,6 @@
 package org.yapp.domain.user.presentation.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +8,6 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserDeleteRequest {
 
+  @Size(max = 100, message = "탈퇴 이유는 100자 이하여야합니다.")
   private String reason;
 }
