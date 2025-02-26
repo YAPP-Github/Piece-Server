fd2307f6b403f126c0d0ffb135efcf48ac3482b9
Chanho Lee <devchlee12@gmail.com>
2025-02-27 00:29:17 +0900

    [PC-684] feat: 유저 기본정보 조회 API 엔드포인트 추가

--- api/src/main/java/org/yapp/domain/user/presentation/UserController.java
+++ api/src/main/java/org/yapp/domain/user/presentation/UserController.java
@@ -16,6 +16,7 @@ import org.yapp.domain.auth.application.oauth.service.OauthService;
 import org.yapp.domain.user.application.UserService;
 import org.yapp.domain.user.presentation.dto.request.OauthUserDeleteRequest;
 import org.yapp.domain.user.presentation.dto.request.UserDeleteRequest;
+import org.yapp.domain.user.presentation.dto.response.UserBasicInfoResponse;
 import org.yapp.domain.user.presentation.dto.response.UserRejectHistoryResponse;
 import org.yapp.format.CommonResponse;
 
@@ -24,39 +25,48 @@ import org.yapp.format.CommonResponse;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserService userService;
-    private final OauthService oauthService;
+  private final UserService userService;
+  private final OauthService oauthService;
 
-    @GetMapping("/reject")
-    @PreAuthorize(value = "hasAnyAuthority('PENDING')")
-    @Operation(summary = "사용자 거절 사유 조회", description = "사용자의 최근 거절 사유를 조회합니다.", tags = {"사용자"})
-    public ResponseEntity<CommonResponse<UserRejectHistoryResponse>> getUserRejectHistory(
-        @AuthenticationPrincipal Long userId) {
-        UserRejectHistoryResponse response = userService.getUserRejectHistoryLatest(
-            userId);
+  @GetMapping("/reject")
+  @PreAuthorize(value = "hasAnyAuthority('PENDING')")
+  @Operation(summary = "사용자 거절 사유 조회", description = "사용자의 최근 거절 사유를 조회합니다.", tags = {"사용자"})
+  public ResponseEntity<CommonResponse<UserRejectHistoryResponse>> getUserRejectHistory(
+      @AuthenticationPrincipal Long userId) {
+    UserRejectHistoryResponse response = userService.getUserRejectHistoryLatest(
+        userId);
 
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(CommonResponse.createSuccess(response));
-    }
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(CommonResponse.createSuccess(response));
+  }
 
-    @DeleteMapping
-    @PreAuthorize(value = "hasAnyAuthority('REGISTER','PENDING','USER')")
-    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴합니다.", tags = {"사용자"})
-    public ResponseEntity<CommonResponse<Void>> deleteUser(
-        @RequestBody @Valid UserDeleteRequest request,
-        @AuthenticationPrincipal Long userId) {
-        userService.deleteUser(userId, request.getReason());
-        return ResponseEntity.ok(CommonResponse.createSuccessWithNoContent());
-    }
+  @DeleteMapping
+  @PreAuthorize(value = "hasAnyAuthority('REGISTER','PENDING','USER')")
+  @Operation(summary = "회원 탈퇴", description = "회원 탈퇴합니다.", tags = {"사용자"})
+  public ResponseEntity<CommonResponse<Void>> deleteUser(
+      @RequestBody @Valid UserDeleteRequest request,
+      @AuthenticationPrincipal Long userId) {
+    userService.deleteUser(userId, request.getReason());
+    return ResponseEntity.ok(CommonResponse.createSuccessWithNoContent());
+  }
 
-    @DeleteMapping("/oauth")
-    @PreAuthorize(value = "hasAnyAuthority('REGISTER','PENDING','USER')")
-    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴합니다.", tags = {"사용자"})
-    public ResponseEntity<CommonResponse<Void>> deleteUser(
-        @RequestBody @Valid OauthUserDeleteRequest request,
-        @AuthenticationPrincipal Long userId) {
+  @DeleteMapping("/oauth")
+  @PreAuthorize(value = "hasAnyAuthority('REGISTER','PENDING','USER')")
+  @Operation(summary = "회원 탈퇴", description = "회원 탈퇴합니다.", tags = {"사용자"})
+  public ResponseEntity<CommonResponse<Void>> deleteUser(
+      @RequestBody @Valid OauthUserDeleteRequest request,
+      @AuthenticationPrincipal Long userId) {
 
-        oauthService.withdraw(request, userId);
-        return ResponseEntity.ok(CommonResponse.createSuccessWithNoContent());
-    }
+    oauthService.withdraw(request, userId);
+    return ResponseEntity.ok(CommonResponse.createSuccessWithNoContent());
+  }
+
+  @GetMapping("/info")
+  @PreAuthorize(value = "hasAnyAuthority('NONE', 'REGISTER', 'PENDING', 'USER')")
+  @Operation(summary = "회원 기본정보 확인", description = "회원의 기본 정보를 확인합니다", tags = {"사용자"})
+  public ResponseEntity<CommonResponse<UserBasicInfoResponse>> getUserBasicInfo(
+      @AuthenticationPrincipal Long userId) {
+    UserBasicInfoResponse userBasicInfo = userService.getUserBasicInfo(userId);
+    return ResponseEntity.ok(CommonResponse.createSuccess(userBasicInfo));
+  }
 }
