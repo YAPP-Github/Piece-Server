df17ec0c76602515576efa66de1d50b40892cfcc
Chanho Lee <devchlee12@gmail.com>
2025-03-01 17:23:23 +0900

    [PC-684] feat: FcmToken 리포지토리 추가

---
+++ api/src/main/java/org/yapp/domain/user/dao/FcmTokenRepository.java
@@ -0,0 +1,12 @@
+package org.yapp.domain.user.dao;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.yapp.core.domain.fcm.FcmToken;
+
+public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+
+  Optional<FcmToken> findByUserId(Long userId);
+
+  void deleteByUserId(Long userId);
+}
