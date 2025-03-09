0eaabe5bbceda014ce976e0f120daf1fb46643f3
Chanho Lee <devchlee12@gmail.com>
2025-03-09 21:04:02 +0900

    [PC-694] fix: 기존 NotificationService 를 CommonNotificationService로 변경

---
+++ core/notification/src/main/java/org/yapp/core/notification/application/CommonNotificationService.java
@@ -0,0 +1,40 @@
+package org.yapp.core.notification.application;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yapp.core.domain.notification.NotificationHistory;
+import org.yapp.core.domain.notification.enums.NotificationType;
+import org.yapp.core.notification.client.NotificationClient;
+import org.yapp.core.notification.dao.NotificationHistoryRepository;
+import org.yapp.core.notification.provider.NotificationProvider;
+
+@Service
+@RequiredArgsConstructor
+public class CommonNotificationService {
+
+  private final NotificationProvider notificationProvider;
+  private final NotificationHistoryRepository notificationHistoryRepository;
+
+  @Transactional
+  public void sendNotification(String clientName, String token, Long userId,
+      NotificationType notificationType, String title, String body) {
+    Map<String, String> data = new HashMap<>();
+    NotificationClient notificationClient = notificationProvider.getNotificationClient(
+        clientName);
+
+    NotificationHistory notificationHistory = new NotificationHistory(userId, notificationType,
+        title, body, false, LocalDateTime.now());
+    NotificationHistory newNotificationHistory = notificationHistoryRepository.save(
+        notificationHistory);
+    data.put("notificationId", String.valueOf(newNotificationHistory.getId()));
+
+    notificationClient.sendNotificationWithData(token, title, body, data);
+  }
+
+
+}
+
