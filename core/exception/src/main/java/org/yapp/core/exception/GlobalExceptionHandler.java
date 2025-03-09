95ae712fbb7641f65c4bf58fc2cf037949e47b05
LujaeC <youjae0803@gmail.com>
2025-03-09 15:34:57 +0900

    [PC-423] feat: 디스코드 알림 발송 (예외 발생시)

--- core/exception/src/main/java/org/yapp/core/exception/GlobalExceptionHandler.java
+++ core/exception/src/main/java/org/yapp/core/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package org.yapp.core.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -17,90 +19,101 @@ import org.yapp.core.exception.error.code.AuthErrorCode;
 import org.yapp.core.exception.error.code.CommonErrorCode;
 import org.yapp.core.exception.error.code.ErrorCode;
 import org.yapp.core.exception.error.response.ErrorResponse;
+import org.yapp.infra.discord.DiscordMessageFactory;
+import org.yapp.infra.discord.DiscordNotificationService;
 
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler(ApplicationException.class)
-  public ResponseEntity<Object> handleQuizException(final ApplicationException e) {
-    final ErrorCode errorCode = e.getErrorCode();
-    return handleExceptionInternal(errorCode);
-  }
+    @Autowired
+    private DiscordNotificationService discordNotificationService;
 
-  @ExceptionHandler(AuthorizationDeniedException.class)
-  public ResponseEntity<Object> handleAuthorizationException(final AuthorizationDeniedException e) {
-    return handleExceptionInternal(AuthErrorCode.ACCESS_DENIED);
-  }
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<Object> handleApplicationException(final ApplicationException e,
+        final HttpServletRequest request) {
+        ErrorCode errorCode = e.getErrorCode();
+        discordNotificationService.sendNotification(
+            DiscordMessageFactory.createErrorMessage(request, e));
+        return handleExceptionInternal(errorCode);
+    }
 
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<Object> handleIllegalArgument(final IllegalArgumentException e) {
-    log.warn("handleIllegalArgument", e);
-    final ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
-    return handleExceptionInternal(errorCode, e.getMessage());
-  }
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<Object> handleAuthorizationException(
+        final AuthorizationDeniedException e, final HttpServletRequest request) {
+        discordNotificationService.sendNotification(
+            DiscordMessageFactory.createErrorMessage(request, e));
+        return handleExceptionInternal(AuthErrorCode.ACCESS_DENIED);
+    }
 
-  @Override
-  public ResponseEntity<Object> handleMethodArgumentNotValid(
-      final MethodArgumentNotValidException e,
-      final HttpHeaders headers,
-      final HttpStatusCode status,
-      final WebRequest request) {
-    log.warn("handleIllegalArgument", e);
-    final ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
-    return handleExceptionInternal(e, errorCode);
-  }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(final IllegalArgumentException e,
+        final HttpServletRequest request) {
+        log.warn("handleIllegalArgument", e);
+        discordNotificationService.sendNotification(
+            DiscordMessageFactory.createErrorMessage(request, e));
+        return handleExceptionInternal(CommonErrorCode.INVALID_PARAMETER, e.getMessage());
+    }
 
-  @ExceptionHandler({Exception.class})
-  public ResponseEntity<Object> handleAllException(final Exception ex) {
-    log.warn("handleAllException", ex);
-    final ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
-    return handleExceptionInternal(errorCode);
-  }
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+        final MethodArgumentNotValidException e,
+        final HttpHeaders headers,
+        final HttpStatusCode status,
+        final WebRequest request) {
+        log.warn("handleMethodArgumentNotValid", e);
+        return handleExceptionInternal(e, CommonErrorCode.INVALID_PARAMETER);
+    }
 
-  private ResponseEntity<Object> handleExceptionInternal(final ErrorCode errorCode) {
-    return ResponseEntity.status(errorCode.getHttpStatus())
-        .body(makeErrorResponse(errorCode));
-  }
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleAllException(final Exception ex) {
+        log.warn("handleAllException", ex);
+        return handleExceptionInternal(CommonErrorCode.INTERNAL_SERVER_ERROR);
+    }
 
-  private ResponseEntity<Object> handleExceptionInternal(final ErrorCode errorCode,
-      final String message) {
-    return ResponseEntity.status(errorCode.getHttpStatus())
-        .body(makeErrorResponse(errorCode, message));
-  }
+    private ResponseEntity<Object> handleExceptionInternal(final ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(errorCode));
+    }
 
-  private ResponseEntity<Object> handleExceptionInternal(final BindException e,
-      final ErrorCode errorCode) {
-    return ResponseEntity.status(errorCode.getHttpStatus())
-        .body(makeErrorResponse(e, errorCode));
-  }
+    private ResponseEntity<Object> handleExceptionInternal(final ErrorCode errorCode,
+        final String message) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(errorCode, message));
+    }
 
-  private ErrorResponse makeErrorResponse(final ErrorCode errorCode) {
-    return ErrorResponse.builder()
-        .code(errorCode.name())
-        .message(errorCode.getMessage())
-        .build();
-  }
+    private ResponseEntity<Object> handleExceptionInternal(final BindException e,
+        final ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(makeErrorResponse(e, errorCode));
+    }
 
-  private ErrorResponse makeErrorResponse(final ErrorCode errorCode, final String message) {
-    return ErrorResponse.builder()
-        .code(errorCode.name())
-        .message(message)
-        .build();
-  }
+    private ErrorResponse makeErrorResponse(final ErrorCode errorCode) {
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .message(errorCode.getMessage())
+            .build();
+    }
 
-  private ErrorResponse makeErrorResponse(final BindException e, final ErrorCode errorCode) {
-    final List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
-        .getFieldErrors()
-        .stream()
-        .map(ErrorResponse.ValidationError::of)
-        .collect(Collectors.toList());
+    private ErrorResponse makeErrorResponse(final ErrorCode errorCode, final String message) {
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .message(message)
+            .build();
+    }
 
-    return ErrorResponse.builder()
-        .code(errorCode.name())
-        .message(errorCode.getMessage())
-        .errors(validationErrorList)
-        .build();
-  }
+    private ErrorResponse makeErrorResponse(final BindException e, final ErrorCode errorCode) {
+        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(ErrorResponse.ValidationError::of)
+            .collect(Collectors.toList());
+
+        return ErrorResponse.builder()
+            .code(errorCode.name())
+            .message(errorCode.getMessage())
+            .errors(validationErrorList)
+            .build();
+    }
 }
