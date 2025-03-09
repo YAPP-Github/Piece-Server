7f36863af211c55bca5c525c52e0b8a96015a4e7
LujaeC <youjae0803@gmail.com>
2025-03-09 15:35:29 +0900

    [PC-423] feat: 디스코드 알림 발송 (매칭 배치 작업 완료시)

--- api/src/main/java/org/yapp/domain/match/batch/MatchScheduler.java
+++ api/src/main/java/org/yapp/domain/match/batch/MatchScheduler.java
@@ -5,18 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.yapp.domain.match.application.matcher.CoupleMatcher;
+import org.yapp.infra.discord.DiscordMessageFactory;
+import org.yapp.infra.discord.DiscordNotificationService;
 
 @Component
 @RequiredArgsConstructor
 public class MatchScheduler {
 
-  private final CoupleMatcher matcher;
+    private final CoupleMatcher matcher;
+    private final DiscordNotificationService discordNotificationService;
 
-  /**
-   * 10시에 매칭을 위해 9시부터 배치 시작
-   */
-  @Scheduled(cron = "0 0 21 * * *")
-  public void match() {
-    matcher.match();
-  }
+    /**
+     * 10시에 매칭을 위해 9시부터 배치 시작
+     */
+    @Scheduled(cron = "0 0 21 * * *")
+    public void match() {
+        matcher.match();
+        discordNotificationService.sendNotification(DiscordMessageFactory.createBasicServiceMessage(
+            null, "매칭 배치 작업", "매칭 배치 작업을 완료하였습니다"
+        ));
+    }
 }
