b5b9a8c57dc2420ccb823a60b4d3b8607d9ef84d
LujaeC <youjae0803@gmail.com>
2025-04-20 14:13:23 +0900

    [PC-856] fix: FcmToken 테이블 created_at, updated_at 추가

--- core/domain/src/main/java/org/yapp/core/domain/fcm/FcmToken.java
+++ core/domain/src/main/java/org/yapp/core/domain/fcm/FcmToken.java
@@ -8,28 +8,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.yapp.core.domain.BaseEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "fcm_token")
-public class FcmToken {
+public class FcmToken extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  private Long id;
-  @Column(name = "user_id")
-  private Long userId;
-  @Column(name = "token")
-  private String token;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "token")
+    private String token;
 
-  public FcmToken(Long userId, String token) {
-    this.userId = userId;
-    this.token = token;
-  }
+    public FcmToken(Long userId, String token) {
+        this.userId = userId;
+        this.token = token;
+    }
 
-  public void updateToken(String token) {
-    this.token = token;
-  }
+    public void updateToken(String token) {
+        this.token = token;
+    }
 }
