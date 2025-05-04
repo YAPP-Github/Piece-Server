45f8e4973ad56181dabaaac4da34f046f6497907
LujaeC <youjae0803@gmail.com>
2025-05-04 17:18:24 +0900

    [PC-876] feat: 프로필 이미지 심사 기능 [Admin]

--- admin/src/main/java/org/yapp/profile/application/AdminProfileService.java
+++ admin/src/main/java/org/yapp/profile/application/AdminProfileService.java
@@ -18,41 +18,51 @@ import org.yapp.user.dao.UserRejectHistoryRepository;
 @RequiredArgsConstructor
 public class AdminProfileService {
 
-  private final UserRejectHistoryRepository userRejectHistoryRepository;
-  private final UserService userService;
-  private final AdminNotificationService adminNotificationService;
+    private final UserRejectHistoryRepository userRejectHistoryRepository;
+    private final UserService userService;
+    private final AdminNotificationService adminNotificationService;
+    private final AdminProfileImageService adminProfileImageService;
 
-  @Transactional
-  public void updateProfileStatus(Long userId, boolean reasonImage, boolean reasonDescription) {
-    User user = userService.getUserById(userId);
-    Profile profile = user.getProfile();
+    @Transactional
+    public void updateProfileStatus(Long userId, boolean reasonImage, boolean reasonDescription) {
+        User user = userService.getUserById(userId);
+        Profile profile = user.getProfile();
 
-    if (profile == null) {
-      throw new ApplicationException(ProfileErrorCode.NOTFOUND_PROFILE);
+        if (profile == null) {
+            throw new ApplicationException(ProfileErrorCode.NOTFOUND_PROFILE);
+        }
+
+        if (reasonImage || reasonDescription) {
+            rejectProfile(user.getProfile(), reasonImage, reasonDescription);
+            adminNotificationService.sendProfileRejectedNotification(userId);
+        } else {
+            passProfile(profile);
+            adminNotificationService.sendProfileApprovedNotification(userId);
+        }
     }
 
-    if (reasonImage || reasonDescription) {
-      rejectProfile(user.getProfile(), reasonImage, reasonDescription);
-      adminNotificationService.sendProfileRejectedNotification(userId);
-    } else {
-      passProfile(profile);
-      adminNotificationService.sendProfileApprovedNotification(userId);
+    private void rejectProfile(Profile profile, boolean reasonImage, boolean reasonDescription) {
+        userRejectHistoryRepository.save(UserRejectHistory.builder()
+            .user(profile.getUser())
+            .reasonImage(reasonImage)
+            .reasonDescription(reasonDescription)
+            .build());
+
+        profile.updateProfileStatus(ProfileStatus.REJECTED);
+        profile.getUser().updateUserRole(RoleStatus.PENDING.getStatus());
     }
-  }
 
-  private void rejectProfile(Profile profile, boolean reasonImage, boolean reasonDescription) {
-    userRejectHistoryRepository.save(UserRejectHistory.builder()
-        .user(profile.getUser())
-        .reasonImage(reasonImage)
-        .reasonDescription(reasonDescription)
-        .build());
+    private void passProfile(Profile profile) {
+        profile.updateProfileStatus(ProfileStatus.APPROVED);
+        profile.getUser().updateUserRole(RoleStatus.USER.getStatus());
+    }
 
-    profile.updateProfileStatus(ProfileStatus.REJECTED);
-    profile.getUser().updateUserRole(RoleStatus.PENDING.getStatus());
-  }
-
-  private void passProfile(Profile profile) {
-    profile.updateProfileStatus(ProfileStatus.APPROVED);
-    profile.getUser().updateUserRole(RoleStatus.USER.getStatus());
-  }
+    @Transactional
+    public void updateProfileImageStatus(Long profileImageId, boolean accepted) {
+        if (accepted) {
+            adminProfileImageService.acceptProfileImage(profileImageId);
+        } else {
+            adminProfileImageService.rejectProfileImage(profileImageId);
+        }
+    }
 }
