b3ea2c4f6a41f566123c84f6920da8e898090fda
Chanho Lee <101349340+devchlee12@users.noreply.github.com>
2025-05-05 23:47:24 +0900

    Revert "[PC-921] SMS 인증코드 버그 픽스 및 IOS 직렬화 이슈 대응"

--- api/src/main/java/org/yapp/domain/auth/application/authorization/SmsAuthService.java
+++ api/src/main/java/org/yapp/domain/auth/application/authorization/SmsAuthService.java
@@ -15,43 +15,43 @@ import org.yapp.infra.redis.application.RedisService;
 @RequiredArgsConstructor
 public class SmsAuthService {
 
-  private static final long AUTH_CODE_EXPIRE_TIME = 300000;
-  private static final String AUTH_CODE_KEY_PREFIX = "authcode:";
-  private static final String AUTH_CODE_INNER_FORMAT = "%06d";
-  private static final String AUTH_CODE_FORMAT = "[PIECE] 인증 번호는 %06d 입니다.";
-  private final AuthCodeGenerator authCodeGenerator;
-  private final SmsSenderService smsSenderService;
-  private final RedisService redisService;
+    private static final long AUTH_CODE_EXPIRE_TIME = 300000;
+    private static final String AUTH_CODE_KEY_PREFIX = "authcode:";
+    private static final String AUTH_CODE_FORMAT = "[PIECE] 인증 번호는 %06d 입니다.";
+    private final AuthCodeGenerator authCodeGenerator;
+    private final SmsSenderService smsSenderService;
+    private final RedisService redisService;
 
-  /**
-   * 인증번호 전송
-   *
-   * @param phoneNumber 인증 번호를 받을 핸드폰 번호
-   */
-  public SmsAuthResponse sendAuthCodeTo(String phoneNumber) {
+    /**
+     * 인증번호 전송
+     *
+     * @param phoneNumber 인증 번호를 받을 핸드폰 번호
+     */
+    public SmsAuthResponse sendAuthCodeTo(String phoneNumber) {
 
-    int authCode = authCodeGenerator.generate();
-    redisService.setKeyWithExpiration(AUTH_CODE_KEY_PREFIX + phoneNumber,
-        String.format(AUTH_CODE_INNER_FORMAT, authCode), AUTH_CODE_EXPIRE_TIME);
-    String authCodeMessage = String.format(AUTH_CODE_FORMAT, authCode);
-    smsSenderService.sendSMS(phoneNumber, authCodeMessage);
-    return new SmsAuthResponse(phoneNumber);
-  }
-
-  /**
-   * 인증번호 인증
-   *
-   * @param phoneNumber 핸드폰번호
-   * @param code        인증 번호
-   * @return 인증번호 일치 여부
-   */
-  public void verifySmsAuthCode(String phoneNumber, String code) {
-    String expectedCode = redisService.getValue(AUTH_CODE_KEY_PREFIX + phoneNumber);
-    if (expectedCode == null) {
-      throw new ApplicationException(SmsAuthErrorCode.CODE_NOT_EXIST);
+        int authCode = authCodeGenerator.generate();
+        redisService.setKeyWithExpiration(AUTH_CODE_KEY_PREFIX + phoneNumber,
+            String.valueOf(authCode),
+            AUTH_CODE_EXPIRE_TIME);
+        String authCodeMessage = String.format(AUTH_CODE_FORMAT, authCode);
+        smsSenderService.sendSMS(phoneNumber, authCodeMessage);
+        return new SmsAuthResponse(phoneNumber);
     }
-    if (!expectedCode.equals(code)) {
-      throw new ApplicationException(SmsAuthErrorCode.CODE_NOT_CORRECT);
+
+    /**
+     * 인증번호 인증
+     *
+     * @param phoneNumber 핸드폰번호
+     * @param code        인증 번호
+     * @return 인증번호 일치 여부
+     */
+    public void verifySmsAuthCode(String phoneNumber, String code) {
+        String expectedCode = redisService.getValue(AUTH_CODE_KEY_PREFIX + phoneNumber);
+        if (expectedCode == null) {
+            throw new ApplicationException(SmsAuthErrorCode.CODE_NOT_EXIST);
+        }
+        if (!expectedCode.equals(code)) {
+            throw new ApplicationException(SmsAuthErrorCode.CODE_NOT_CORRECT);
+        }
     }
-  }
 }
--- api/src/main/java/org/yapp/domain/setting/dto/response/BlockContactSyncTimeResponse.java
+++ api/src/main/java/org/yapp/domain/setting/dto/response/BlockContactSyncTimeResponse.java
@@ -1,15 +1,14 @@
 package org.yapp.domain.setting.dto.response;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.yapp.domain.setting.dto.serializer.BlockContactSyncTimeResponseSerializer;
 
 @Getter
 @AllArgsConstructor
 public class BlockContactSyncTimeResponse {
 
-  @JsonSerialize(using = BlockContactSyncTimeResponseSerializer.class)
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
   private LocalDateTime syncTime;
 }
