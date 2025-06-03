4eb6dfdf0a470b99dcffdd6b81631014cee19ee2
Chanho Lee <devchlee12@gmail.com>
2025-06-03 21:00:54 +0900

    feat: 즉시매칭 벡터 생성기 추가

--- api/src/main/java/org/yapp/domain/match/application/InstantMatchVectorGenerator.java
+++ api/src/main/java/org/yapp/domain/match/application/InstantMatchVectorGenerator.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.yapp.core.domain.profile.Profile;
 import org.yapp.core.domain.profile.ProfileValuePick;
-import org.yapp.core.domain.user.User;
 import org.yapp.domain.profile.application.ProfileValuePickService;
 import org.yapp.domain.user.application.UserService;
 
@@ -15,28 +14,26 @@ import org.yapp.domain.user.application.UserService;
 @RequiredArgsConstructor
 public class InstantMatchVectorGenerator {
 
-  private final ProfileValuePickService profileValuePickService;
-  private final UserService userService;
+    private final ProfileValuePickService profileValuePickService;
+    private final UserService userService;
 
-  private List<Double> oneHotEncode(int value) {
-    List<Double> oneHot = Arrays.asList(0D, 0D, 0D);
-    oneHot.set(value - 1, 1D);
-    return oneHot;
-  }
-
-  public List<Double> generate(Long userId) {
-    User user = userService.getUserById(userId);
-    Profile profile = user.getProfile();
-    List<ProfileValuePick> valuePicks = profileValuePickService.getAllProfileValuePicksByProfileId(
-        profile.getId());
-
-    List<Double> vector = new ArrayList<>();
-    for (ProfileValuePick valuePick : valuePicks) {
-      Integer selectedAnswer = valuePick.getSelectedAnswer();
-      List<Double> encodedValue = oneHotEncode(selectedAnswer);
-      vector.addAll(encodedValue);
+    private List<Double> oneHotEncode(int value) {
+        List<Double> oneHot = Arrays.asList(0D, 0D, 0D);
+        oneHot.set(value - 1, 1D);
+        return oneHot;
     }
 
-    return vector;
-  }
+    public List<Double> generate(Profile profile) {
+        List<ProfileValuePick> valuePicks = profileValuePickService.getAllProfileValuePicksByProfileId(
+            profile.getId());
+
+        List<Double> vector = new ArrayList<>();
+        for (ProfileValuePick valuePick : valuePicks) {
+            Integer selectedAnswer = valuePick.getSelectedAnswer();
+            List<Double> encodedValue = oneHotEncode(selectedAnswer);
+            vector.addAll(encodedValue);
+        }
+
+        return vector;
+    }
 }
