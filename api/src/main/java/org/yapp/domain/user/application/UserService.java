2f01e1c631c61bb25061987f3b8394c134845ab6
Chanho Lee <devchlee12@gmail.com>
2025-06-13 23:47:29 +0900

    [PC-1007] feat: role 변경 체크하고 토큰 재발급 로직 구현

--- api/src/main/java/org/yapp/domain/user/application/UserService.java
+++ api/src/main/java/org/yapp/domain/user/application/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.yapp.core.auth.AuthToken;
 import org.yapp.core.auth.AuthTokenGenerator;
 import org.yapp.core.auth.dao.BannedUserPhoneNumberRepository;
+import org.yapp.core.auth.jwt.JwtUtil;
 import org.yapp.core.domain.fcm.FcmToken;
 import org.yapp.core.domain.profile.Profile;
 import org.yapp.core.domain.user.RoleStatus;
@@ -31,139 +32,152 @@ import org.yapp.domain.user.presentation.dto.response.UserRejectHistoryResponse;
 @RequiredArgsConstructor
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final UserRejectHistoryRepository userRejectHistoryRepository;
-  private final UserDeleteReasonRepository userDeleteReasonRepository;
-  private final AuthTokenGenerator authTokenGenerator;
-  private final FcmTokenRepository fcmTokenRepository;
-  private final BannedUserPhoneNumberRepository bannedUserPhoneNumberRepository;
+    private final UserRepository userRepository;
+    private final UserRejectHistoryRepository userRejectHistoryRepository;
+    private final UserDeleteReasonRepository userDeleteReasonRepository;
+    private final AuthTokenGenerator authTokenGenerator;
+    private final FcmTokenRepository fcmTokenRepository;
+    private final BannedUserPhoneNumberRepository bannedUserPhoneNumberRepository;
+    private final JwtUtil jwtUtil;
 
-  /**
-   * Role을 USER로 바꾸고 변경된 토큰을 반환한다.
-   *
-   * @return 액세스토큰과 리프레시 토큰
-   */
-  @Transactional
-  public OauthLoginResponse completeProfileInitialize(Long userId, Profile profile) {
-    User user =
-        userRepository.findById(userId)
+    /**
+     * Role을 USER로 바꾸고 변경된 토큰을 반환한다.
+     *
+     * @return 액세스토큰과 리프레시 토큰
+     */
+    @Transactional
+    public OauthLoginResponse completeProfileInitialize(Long userId, Profile profile) {
+        User user =
+            userRepository.findById(userId)
+                .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
+        user.setProfile(profile);
+        user.updateUserRole(RoleStatus.PENDING.getStatus());
+        String oauthId = user.getOauthId();
+        AuthToken authToken = authTokenGenerator.generate(userId, oauthId, user.getRole());
+        return new OauthLoginResponse(RoleStatus.PENDING.getStatus(), authToken.accessToken(),
+            authToken.refreshToken());
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
             .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
-    user.setProfile(profile);
-    user.updateUserRole(RoleStatus.PENDING.getStatus());
-    String oauthId = user.getOauthId();
-    AuthToken authToken = authTokenGenerator.generate(userId, oauthId, user.getRole());
-    return new OauthLoginResponse(RoleStatus.PENDING.getStatus(), authToken.accessToken(),
-        authToken.refreshToken());
-  }
-
-  public User getUserById(Long userId) {
-    return userRepository.findById(userId)
-        .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
-  }
-
-  /**
-   * Role을 Register로 바꾸고 변경된 토큰을 반환한다.
-   *
-   * @return 액세스토큰과 리프레시 토큰
-   */
-  @Transactional
-  public SmsVerifyResponse registerPhoneNumber(Long userId, String phoneNumber) {
-    Optional<User> userOptionalByPhoneNumber = this.getUserByPhoneNumber(phoneNumber);
-
-    // 이미 가입한 유저
-    if (userOptionalByPhoneNumber.isPresent()) {
-      String userOauthProvider = this.getUserOauthProvider(
-          userOptionalByPhoneNumber.get().getOauthId());
-
-      return new SmsVerifyResponse(null, null, null, true, userOauthProvider);
     }
 
-    // 정지된 유저가 기존 계정 삭제하고 다시 가입하려고 할 때 방지
-    if (bannedUserPhoneNumberRepository.findById(phoneNumber).isPresent()) {
-      throw new ApplicationException(AuthErrorCode.PERMANENTLY_BANNED);
+    /**
+     * Role을 Register로 바꾸고 변경된 토큰을 반환한다.
+     *
+     * @return 액세스토큰과 리프레시 토큰
+     */
+    @Transactional
+    public SmsVerifyResponse registerPhoneNumber(Long userId, String phoneNumber) {
+        Optional<User> userOptionalByPhoneNumber = this.getUserByPhoneNumber(phoneNumber);
+
+        // 이미 가입한 유저
+        if (userOptionalByPhoneNumber.isPresent()) {
+            String userOauthProvider = this.getUserOauthProvider(
+                userOptionalByPhoneNumber.get().getOauthId());
+
+            return new SmsVerifyResponse(null, null, null, true, userOauthProvider);
+        }
+
+        // 정지된 유저가 기존 계정 삭제하고 다시 가입하려고 할 때 방지
+        if (bannedUserPhoneNumberRepository.findById(phoneNumber).isPresent()) {
+            throw new ApplicationException(AuthErrorCode.PERMANENTLY_BANNED);
+        }
+
+        User user =
+            userRepository.findById(userId)
+                .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
+
+        user.updateUserRole(RoleStatus.REGISTER.getStatus());
+        user.initializePhoneNumber(phoneNumber);
+        String oauthId = user.getOauthId();
+        AuthToken authToken = authTokenGenerator.generate(userId, oauthId, user.getRole());
+        return new SmsVerifyResponse(RoleStatus.REGISTER.getStatus(), authToken.accessToken(),
+            authToken.refreshToken(), false, null);
     }
 
-    User user =
-        userRepository.findById(userId)
-            .orElseThrow(() -> new ApplicationException(UserErrorCode.NOTFOUND_USER));
+    @Transactional(readOnly = true)
+    public UserRejectHistoryResponse getUserRejectHistoryLatest(Long userId) {
+        boolean reasonImage = false;
+        boolean reasonDescription = false;
 
-    user.updateUserRole(RoleStatus.REGISTER.getStatus());
-    user.initializePhoneNumber(phoneNumber);
-    String oauthId = user.getOauthId();
-    AuthToken authToken = authTokenGenerator.generate(userId, oauthId, user.getRole());
-    return new SmsVerifyResponse(RoleStatus.REGISTER.getStatus(), authToken.accessToken(),
-        authToken.refreshToken(), false, null);
-  }
+        UserRejectHistory userRejectHistory = userRejectHistoryRepository.findTopByUserIdOrderByCreatedAtDesc(
+            userId).orElse(null);
 
-  @Transactional(readOnly = true)
-  public UserRejectHistoryResponse getUserRejectHistoryLatest(Long userId) {
-    boolean reasonImage = false;
-    boolean reasonDescription = false;
+        if (userRejectHistory != null) {
+            reasonImage = userRejectHistory.isReasonImage();
+            reasonDescription = userRejectHistory.isReasonDescription();
+        }
 
-    UserRejectHistory userRejectHistory = userRejectHistoryRepository.findTopByUserIdOrderByCreatedAtDesc(
-        userId).orElse(null);
-
-    if (userRejectHistory != null) {
-      reasonImage = userRejectHistory.isReasonImage();
-      reasonDescription = userRejectHistory.isReasonDescription();
+        return new UserRejectHistoryResponse(
+            reasonImage,
+            reasonDescription
+        );
     }
 
-    return new UserRejectHistoryResponse(
-        reasonImage,
-        reasonDescription
-    );
-  }
-
-  @Transactional
-  public void deleteUser(Long userId, String reason) {
-    userDeleteReasonRepository.save(new UserDeleteReason(userId, reason));
-    userRepository.deleteById(userId);
-  }
-
-  public UserBasicInfoResponse getUserBasicInfo(Long userId) {
-    User user = this.getUserById(userId);
-
-    Profile profile = user.getProfile();
-    String profileStatus =
-        profile != null ? profile.getProfileStatus().toString() : null;
-
-    return new UserBasicInfoResponse(userId, user.getRole(), profileStatus);
-  }
-
-  @Transactional
-  public void saveFcmToken(Long userId, FcmTokenSaveRequest request) {
-    Optional<FcmToken> fcmTokenOptional = fcmTokenRepository.findByUserId(userId);
-    if (fcmTokenOptional.isPresent()) {
-      FcmToken fcmToken = fcmTokenOptional.get();
-      fcmToken.updateToken(request.getToken());
-    } else {
-      FcmToken fcmToken = new FcmToken(userId, request.getToken());
-      fcmTokenRepository.save(fcmToken);
+    @Transactional
+    public void deleteUser(Long userId, String reason) {
+        userDeleteReasonRepository.save(new UserDeleteReason(userId, reason));
+        userRepository.deleteById(userId);
     }
-  }
 
-  @Transactional
-  public void deleteFcmToken(Long userId) {
-    fcmTokenRepository.deleteByUserId(userId);
-  }
+    public UserBasicInfoResponse getUserBasicInfo(Long userId, String authorizationHeader) {
+        User user = this.getUserById(userId);
 
-  public Optional<User> getUserByPhoneNumber(String phoneNumber) {
-    return userRepository.findByPhoneNumber(phoneNumber);
-  }
+        Profile profile = user.getProfile();
+        String profileStatus =
+            profile != null ? profile.getProfileStatus().toString() : null;
 
-  public String getUserOauthProvider(String oauthId) {
-    if (oauthId.startsWith("kakao")) {
-      return "kakao";
-    } else if (oauthId.startsWith("apple")) {
-      return "apple";
-    } else if (oauthId.startsWith("google")) {
-      return "google";
-    } else {
-      throw new ApplicationException(UserErrorCode.INVALID_OAUTH_PROVIDER);
+        String accessToken = authorizationHeader.substring(7);
+        String accessTokenRole = jwtUtil.getRole(accessToken);
+        String userRole = user.getRole();
+
+        if (!accessTokenRole.equals(userRole)) {
+            AuthToken authToken = authTokenGenerator.generate(userId, user.getOauthId(),
+                user.getRole());
+            return new UserBasicInfoResponse(userId, userRole, profileStatus, true,
+                authToken.accessToken(), authToken.refreshToken());
+        }
+
+        return new UserBasicInfoResponse(userId, userRole, profileStatus, false, null, null);
+
     }
-  }
 
-  public List<User> getAllUsers() {
-    return userRepository.findAll();
-  }
+    @Transactional
+    public void saveFcmToken(Long userId, FcmTokenSaveRequest request) {
+        Optional<FcmToken> fcmTokenOptional = fcmTokenRepository.findByUserId(userId);
+        if (fcmTokenOptional.isPresent()) {
+            FcmToken fcmToken = fcmTokenOptional.get();
+            fcmToken.updateToken(request.getToken());
+        } else {
+            FcmToken fcmToken = new FcmToken(userId, request.getToken());
+            fcmTokenRepository.save(fcmToken);
+        }
+    }
+
+    @Transactional
+    public void deleteFcmToken(Long userId) {
+        fcmTokenRepository.deleteByUserId(userId);
+    }
+
+    public Optional<User> getUserByPhoneNumber(String phoneNumber) {
+        return userRepository.findByPhoneNumber(phoneNumber);
+    }
+
+    public String getUserOauthProvider(String oauthId) {
+        if (oauthId.startsWith("kakao")) {
+            return "kakao";
+        } else if (oauthId.startsWith("apple")) {
+            return "apple";
+        } else if (oauthId.startsWith("google")) {
+            return "google";
+        } else {
+            throw new ApplicationException(UserErrorCode.INVALID_OAUTH_PROVIDER);
+        }
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
