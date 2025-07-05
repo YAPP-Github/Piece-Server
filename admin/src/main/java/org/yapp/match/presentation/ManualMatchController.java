c691146b069cd4ddecd77056832c3a0b5d182752
Chanho Lee <devchlee12@gmail.com>
2025-07-05 23:33:49 +0900

    [PC-894] feat: 수동매칭 관련 조회 API 추가

---
+++ admin/src/main/java/org/yapp/match/presentation/ManualMatchController.java
@@ -0,0 +1,41 @@
+package org.yapp.match.presentation;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.yapp.format.CommonResponse;
+import org.yapp.match.application.query.ManualMatchHistoryQueryService;
+import org.yapp.match.application.query.ManualMatchUserQueryService;
+import org.yapp.match.dto.response.ManualMatchCandidateListResponse;
+import org.yapp.match.dto.response.ManualMatchHistoryResponse;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin/v1/manual-match")
+public class ManualMatchController {
+
+    private final ManualMatchHistoryQueryService manualMatchHistoryQueryService;
+    private final ManualMatchUserQueryService manualMatchUserQueryService;
+
+    @GetMapping("/candidates")
+    public ResponseEntity<CommonResponse<ManualMatchCandidateListResponse>> getManualMatchingCandidateList(
+        @RequestParam(defaultValue = "0", name = "page") int page) {
+        ManualMatchCandidateListResponse candidateList =
+            manualMatchUserQueryService.getCandidateList(page);
+
+        return ResponseEntity.ok(CommonResponse.createSuccess(candidateList));
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<CommonResponse<List<ManualMatchHistoryResponse>>> getManualMatchingHistory(
+        @RequestParam(defaultValue = "0", name = "page") int page) {
+        List<ManualMatchHistoryResponse> manualMatchHistory =
+            manualMatchHistoryQueryService.getManualMatchHistory(page);
+
+        return ResponseEntity.ok(CommonResponse.createSuccess(manualMatchHistory));
+    }
+}
--- core/domain/src/main/java/org/yapp/core/domain/match/ManualMatchHistory.java
+++ core/domain/src/main/java/org/yapp/core/domain/match/ManualMatchHistory.java
@@ -6,8 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class ManualMatchHistory {
 
     @Id
@@ -25,4 +29,12 @@ public class ManualMatchHistory {
 
     @Column(name = "is_matched")
     private Boolean isMatched;
+
+    public ManualMatchHistory(Long user1Id, Long user2Id, LocalDateTime dateTime,
+        Boolean isMatched) {
+        this.user1Id = user1Id;
+        this.user2Id = user2Id;
+        this.dateTime = dateTime;
+        this.isMatched = isMatched;
+    }
 }
