6033b5d59591fad32b36eb7a8c55375b2877adb3
Chanho Lee <devchlee12@gmail.com>
2025-07-13 15:51:05 +0900

    [PC-1060] fix: 영구정지 기능 예외처리

--- api/src/main/java/org/yapp/domain/user/application/BanCheckingService.java
+++ api/src/main/java/org/yapp/domain/user/application/BanCheckingService.java
@@ -5,24 +5,37 @@ import org.springframework.stereotype.Service;
 import org.yapp.core.auth.jwt.JwtUtil;
 import org.yapp.core.exception.ApplicationException;
 import org.yapp.core.exception.error.code.AuthErrorCode;
+import org.yapp.core.exception.error.code.SecurityErrorCode;
 import org.yapp.infra.redis.application.RedisService;
 
 @Service
 @RequiredArgsConstructor
 public class BanCheckingService {
 
-  private static final String USER_BLACK_LIST_SET = "user_blacklist";
+    private static final String USER_BLACK_LIST_SET = "user_blacklist";
 
-  private final RedisService redisService;
-  private final JwtUtil jwtUtil;
+    private final RedisService redisService;
+    private final JwtUtil jwtUtil;
 
-  public boolean existsInBlackList(Long userId) {
-    return redisService.existsInSet(USER_BLACK_LIST_SET, String.valueOf(userId));
-  }
+    public void checkBlackListByRefreshToken(String refreshToken) {
+        checkRefreshTokenExpiration(refreshToken);
+        Long userId = jwtUtil.getUserId(refreshToken);
 
-  public void checkBlackListByRefreshToken(String refreshToken) {
-    if (existsInBlackList(jwtUtil.getUserId(refreshToken))) {
-      throw new ApplicationException(AuthErrorCode.PERMANENTLY_BANNED);
+        boolean isBlocked = existsInBlackList(userId);
+        if (isBlocked) {
+            throw new ApplicationException(AuthErrorCode.PERMANENTLY_BANNED);
+        }
     }
-  }
+
+    private void checkRefreshTokenExpiration(String refreshToken) {
+        try {
+            jwtUtil.isExpired(refreshToken);
+        } catch (Exception e) {
+            throw new ApplicationException(SecurityErrorCode.EXPIRED_REFRESH_TOKEN);
+        }
+    }
+
+    public boolean existsInBlackList(Long userId) {
+        return redisService.existsInSet(USER_BLACK_LIST_SET, String.valueOf(userId));
+    }
 }
