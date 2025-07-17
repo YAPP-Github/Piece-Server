d19a2ebece4ac1747fe591f17f3efbf24d03da0a
LujaeC <youjae0803@gmail.com>
2025-07-17 23:47:42 +0900

    [PC-937] feat: 유저 엔티티에 퍼즐 추가

--- core/domain/src/main/java/org/yapp/core/domain/user/User.java
+++ core/domain/src/main/java/org/yapp/core/domain/user/User.java
@@ -2,6 +2,7 @@ package org.yapp.core.domain.user;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.yapp.core.domain.BaseEntity;
+import org.yapp.core.domain.common.vo.Puzzle;
 import org.yapp.core.domain.profile.Profile;
 
 @Table(name = "user_table")
@@ -24,40 +26,50 @@ import org.yapp.core.domain.profile.Profile;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-  @Id
-  @Column(name = "user_id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(name = "oauth_id")
-  private String oauthId;
+    @Column(name = "oauth_id")
+    private String oauthId;
 
-  @Column(name = "name")
-  private String name;
+    @Column(name = "name")
+    private String name;
 
-  @Column(name = "phone")
-  private String phoneNumber;
+    @Column(name = "phone")
+    private String phoneNumber;
 
-  @Column(name = "role")
-  private String role;
+    @Column(name = "role")
+    private String role;
 
-  @OneToOne(cascade = {CascadeType.REMOVE})
-  @JoinColumn(name = "profile_id", unique = true)  // User가 profile_id를 FK로 가짐
-  private Profile profile;
+    @OneToOne(cascade = {CascadeType.REMOVE})
+    @JoinColumn(name = "profile_id", unique = true) // User가 profile_id를 FK로 가짐
+    private Profile profile;
 
-  @Column(name = "is_admin")
-  private Boolean isAdmin;
+    @Column(name = "is_admin")
+    private Boolean isAdmin;
 
-  public void initializePhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
+    @Embedded
+    private Puzzle puzzleWallet;
 
-  public void setProfile(Profile profile) {
-    this.profile = profile;
-  }
+    public void initializePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-  public void updateUserRole(String role) {
-    this.role = role;
-  }
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public void updateUserRole(String role) {
+        this.role = role;
+    }
+
+    public void addPuzzle(Puzzle puzzle) {
+        if (this.puzzleWallet == null) {
+            this.puzzleWallet = Puzzle.of(0L);
+        }
+
+        this.puzzleWallet = this.puzzleWallet.add(puzzle);
+    }
 }
-
