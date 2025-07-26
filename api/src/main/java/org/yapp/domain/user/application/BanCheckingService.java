42345802f9de44601c7d9d6ea3a91df081bc4964
Chanho Lee <devchlee12@gmail.com>
2025-07-26 15:30:46 +0900

    [PC-1060] refactor: 중복로직 제거

--- api/src/main/java/org/yapp/domain/user/application/BanCheckingService.java
+++ api/src/main/java/org/yapp/domain/user/application/BanCheckingService.java
@@ -3,9 +3,9 @@ package org.yapp.domain.user.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.yapp.core.auth.jwt.JwtUtil;
+import org.yapp.core.auth.token.TokenHealthCheckService;
 import org.yapp.core.exception.ApplicationException;
 import org.yapp.core.exception.error.code.AuthErrorCode;
-import org.yapp.core.exception.error.code.SecurityErrorCode;
 import org.yapp.infra.redis.application.RedisService;
 
 @Service
@@ -13,25 +13,18 @@ import org.yapp.infra.redis.application.RedisService;
 public class BanCheckingService {
 
     private static final String USER_BLACK_LIST_SET = "user_blacklist";
-
+    
     private final RedisService redisService;
     private final JwtUtil jwtUtil;
+    private final TokenHealthCheckService tokenHealthCheckService;
 
     public void checkBlackListByRefreshToken(String refreshToken) {
-        checkRefreshTokenExpiration(refreshToken);
+        tokenHealthCheckService.healthCheck(refreshToken);
         Long userId = jwtUtil.getUserId(refreshToken);
 
         boolean isBlocked = existsInBlackList(userId);
         if (isBlocked) {
             throw new ApplicationException(AuthErrorCode.PERMANENTLY_BANNED);
-        }
-    }
-
-    private void checkRefreshTokenExpiration(String refreshToken) {
-        try {
-            jwtUtil.isExpired(refreshToken);
-        } catch (Exception e) {
-            throw new ApplicationException(SecurityErrorCode.EXPIRED_REFRESH_TOKEN);
         }
     }
 
