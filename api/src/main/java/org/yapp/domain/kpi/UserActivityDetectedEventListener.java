b1070dab1748df1db47fa78ddca5a36a07540095
LujaeC <youjae0803@gmail.com>
2025-07-26 12:47:06 +0900

    refactor: RedisDauService를 사용하도록 수정

--- api/src/main/java/org/yapp/domain/kpi/UserActivityDetectedEventListener.java
+++ api/src/main/java/org/yapp/domain/kpi/UserActivityDetectedEventListener.java
@@ -1,31 +1,25 @@
 package org.yapp.domain.kpi;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.yapp.core.domain.kpi.event.UserActivityDetectedEvent;
+import org.yapp.infra.redis.application.RedisDauService;
 
 @Component
 @RequiredArgsConstructor
 public class UserActivityDetectedEventListener {
 
-    private final RedisTemplate<String, String> redisTemplate;
-
-    private static final String DAU_KEY_PREFIX = "DAU:";
-
+    private final RedisDauService redisDauService;
+    
     @Async
     @EventListener
     public void onUserActivityDetected(UserActivityDetectedEvent event) {
         if (event.getUserId() == null) {
             return;
         }
-        String dailyKey = DAU_KEY_PREFIX + LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
-        redisTemplate.opsForValue().setBit(dailyKey, event.getUserId(), true);
-        redisTemplate.expire(dailyKey, 7, TimeUnit.DAYS);
+        redisDauService.setUserActive(event.getUserId(), LocalDate.now());
     }
 }
