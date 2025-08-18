e5c6b2e9c12066c4719ac9e96a4619f7930bdc4a
LujaeC <youjae0803@gmail.com>
2025-08-18 22:40:53 +0900

    feat: 플레이 스토어 영수증 검증 기능

--- api/src/main/java/org/yapp/global/config/GoogleOauthConfig.java
+++ api/src/main/java/org/yapp/global/config/GoogleOauthConfig.java
@@ -3,24 +3,23 @@ package org.yapp.global.config;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
-
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @RequiredArgsConstructor
 public class GoogleOauthConfig {
-  @Value("${app.google.client.id}")
-  private String googleClientId;
 
-  @Bean
-  public GoogleIdTokenVerifier googleIdTokenVerifier() {
-    return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance()).setAudience(
-        Collections.singletonList(googleClientId)).build();
-  }
+    @Value("${app.google.client.android.id}")
+    private String googleClientId;
+
+    @Bean
+    public GoogleIdTokenVerifier googleIdTokenVerifier() {
+        return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(),
+            JacksonFactory.getDefaultInstance()).setAudience(
+            Collections.singletonList(googleClientId)).build();
+    }
 }
