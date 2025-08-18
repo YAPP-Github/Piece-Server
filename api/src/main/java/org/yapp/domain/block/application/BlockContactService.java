f201be2e7187ddc5f22caddc861ecd20cc1a1eef
Chanho Lee <devchlee12@gmail.com>
2025-08-18 22:14:53 +0900

    [PC-1136] fix: 차단시 중복 전화번호 정상 처리하도록 처리

--- api/src/main/java/org/yapp/domain/block/application/BlockContactService.java
+++ api/src/main/java/org/yapp/domain/block/application/BlockContactService.java
@@ -2,6 +2,7 @@ package org.yapp.domain.block.application;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,44 +19,45 @@ import org.yapp.domain.setting.application.BlockContactSyncTimeService;
 @RequiredArgsConstructor
 public class BlockContactService {
 
-  private final BlockContactRepository blockContactRepository;
-  private final BlockContactSyncTimeService blockContactSyncTimeService;
-  private final BloomBlockService bloomBlockService;
+    private final BlockContactRepository blockContactRepository;
+    private final BlockContactSyncTimeService blockContactSyncTimeService;
+    private final BloomBlockService bloomBlockService;
 
-  @Transactional()
-  public void blockPhoneNumbers(BlockContactCreateDto blockContactCreateDto) {
-    Long userId = blockContactCreateDto.userId();
-    List<String> phoneNumbers = blockContactCreateDto.phoneNumbers();
-    List<BlockContact> newBlockContacts = new ArrayList<>();
-    List<String> newPhoneNumbers = new ArrayList<>();
+    @Transactional()
+    public void blockPhoneNumbers(BlockContactCreateDto blockContactCreateDto) {
+        Long userId = blockContactCreateDto.userId();
+        List<String> phoneNumberList = blockContactCreateDto.phoneNumbers();
+        Set<String> phoneNumbers = new HashSet<>(phoneNumberList);
+        List<BlockContact> newBlockContacts = new ArrayList<>();
+        List<String> newPhoneNumbers = new ArrayList<>();
 
-    Set<String> blockedPhoneNumbers = blockContactRepository.findBlocksByUserId(userId)
-        .stream()
-        .map(BlockContact::getPhoneNumber)
-        .collect(Collectors.toSet());
+        Set<String> blockedPhoneNumbers = blockContactRepository.findBlocksByUserId(userId)
+            .stream()
+            .map(BlockContact::getPhoneNumber)
+            .collect(Collectors.toSet());
 
-    phoneNumbers.stream()
-        .filter(phoneNumber -> !blockedPhoneNumbers.contains(phoneNumber))
-        .forEach(phoneNumber -> {
-          BlockContact blockContact = BlockContact.builder()
-              .user(User.builder().id(userId).build())
-              .phoneNumber(phoneNumber)
-              .build();
-          newPhoneNumbers.add(phoneNumber);
-          newBlockContacts.add(blockContact);
-        });
+        phoneNumbers.stream()
+            .filter(phoneNumber -> !blockedPhoneNumbers.contains(phoneNumber))
+            .forEach(phoneNumber -> {
+                BlockContact blockContact = BlockContact.builder()
+                    .user(User.builder().id(userId).build())
+                    .phoneNumber(phoneNumber)
+                    .build();
+                newPhoneNumbers.add(phoneNumber);
+                newBlockContacts.add(blockContact);
+            });
 
-    bloomBlockService.blockContactList(userId, newPhoneNumbers);
-    blockContactSyncTimeService.saveBlockContactSyncTime(userId, LocalDateTime.now());
-    blockContactRepository.saveAll(newBlockContacts);
-  }
+        bloomBlockService.blockContactList(userId, newPhoneNumbers);
+        blockContactSyncTimeService.saveBlockContactSyncTime(userId, LocalDateTime.now());
+        blockContactRepository.saveAll(newBlockContacts);
+    }
 
-  @Transactional(readOnly = true)
-  public List<BlockContact> findBlocksByUserId(Long userId) {
-    return blockContactRepository.findBlocksByUserId(userId);
-  }
+    @Transactional(readOnly = true)
+    public List<BlockContact> findBlocksByUserId(Long userId) {
+        return blockContactRepository.findBlocksByUserId(userId);
+    }
 
-  public boolean checkIfUserBlockedPhoneNumber(Long userId, String phoneNumber) {
-    return blockContactRepository.existsByUser_IdAndPhoneNumber(userId, phoneNumber);
-  }
+    public boolean checkIfUserBlockedPhoneNumber(Long userId, String phoneNumber) {
+        return blockContactRepository.existsByUser_IdAndPhoneNumber(userId, phoneNumber);
+    }
 }
