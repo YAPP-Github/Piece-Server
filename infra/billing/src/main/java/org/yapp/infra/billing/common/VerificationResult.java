6fd9ed3b20c2ced6f9a60cf8d8ff1e03ae7acd5e
LujaeC <youjae0803@gmail.com>
2025-08-18 22:38:39 +0900

    fix: 영수증 검증 실패시 전달되는 VO 수정

--- infra/billing/src/main/java/org/yapp/infra/billing/common/VerificationResult.java
+++ infra/billing/src/main/java/org/yapp/infra/billing/common/VerificationResult.java
@@ -4,17 +4,18 @@ import org.yapp.core.domain.payment.inApp.vo.purchaseCredential.StorePurchaseCre
 import org.yapp.core.domain.payment.inApp.vo.purchaseId.StorePurchaseId;
 
 public record VerificationResult(
-        boolean isSuccess,
-        StorePurchaseId purchaseId,
-        StorePurchaseCredential purchaseCredential,
-        String productId) {
+    boolean isSuccess,
+    StorePurchaseId purchaseId,
+    StorePurchaseCredential purchaseCredential,
+    String productId) {
 
     public static VerificationResult success(StorePurchaseId purchaseId,
-            StorePurchaseCredential purchaseCredential, String productId) {
+        StorePurchaseCredential purchaseCredential, String productId) {
         return new VerificationResult(true, purchaseId, purchaseCredential, productId);
     }
 
-    public static VerificationResult failure(StorePurchaseCredential purchaseCredential) {
-        return new VerificationResult(false, null, purchaseCredential, null);
+    public static VerificationResult failure(StorePurchaseId purchaseId,
+        StorePurchaseCredential purchaseCredential, String productId) {
+        return new VerificationResult(false, purchaseId, purchaseCredential, productId);
     }
 }
