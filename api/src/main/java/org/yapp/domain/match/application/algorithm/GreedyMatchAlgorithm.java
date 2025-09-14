2df971b4706dec9d319eb24b3a911ed6d6f8a1b5
Chanho Lee <devchlee12@gmail.com>
2025-09-15 00:04:26 +0900

    [PC-1254] fix: 가중치 계산 로직 파이프라인 방식으로 변경

--- api/src/main/java/org/yapp/domain/match/application/algorithm/GreedyMatchAlgorithm.java
+++ api/src/main/java/org/yapp/domain/match/application/algorithm/GreedyMatchAlgorithm.java
@@ -22,76 +22,80 @@ import org.yapp.domain.match.application.algorithm.preference.PreferenceCalculat
 @Component
 public class GreedyMatchAlgorithm implements MatchAlgorithm {
 
-  private final MatchService matchService;
-  private final MatchBlocker matchBlocker;
-  private final PreferenceCalculatingPolicy preferenceCalculatingPolicy;
-
-  @Override
-  @Transactional
-  public List<Profile> doMatch(List<Profile> profiles) {
-    PriorityQueue<Edge> priorityEdges = getPriorityEdges(profiles);
-    Set<Long> matchedSet = greedyMatch(priorityEdges);
-    List<Profile> unmatchedProfiles = new ArrayList<>();
-    for (Profile profile : profiles) {
-      if (!matchedSet.contains(profile.getUser().getId())) {
-        unmatchedProfiles.add(profile);
-      }
-    }
-    return unmatchedProfiles;
-  }
-
-  private Set<Long> greedyMatch(PriorityQueue<Edge> priorityEdges) {
-    Set<Long> matchedSet = new HashSet<>();
-    while (!priorityEdges.isEmpty()) {
-      Edge edge = priorityEdges.poll();
-      if (matchedSet.contains(edge.user1Id) || matchedSet.contains(edge.user2Id)) {
-        continue;
-      }
-      matchedSet.add(edge.user1Id);
-      matchedSet.add(edge.user2Id);
-
-      matchService.createMatchInfo(edge.user1Id, edge.user2Id, false);
-    }
-    return matchedSet;
-  }
-
-  private PriorityQueue<Edge> getPriorityEdges(List<Profile> profiles) {
-    PriorityQueue<Edge> priorityEdgeQueue = new PriorityQueue<Edge>();
-    for (Profile profile1 : profiles) {
-      for (Profile profile2 : profiles) {
-        if (profile1.getId().equals(profile2.getId())) {
-          continue;
-        }
-        if (checkBlock(profile1, profile2)) {
-          continue;
-        }
-        int weight = calculateWeight(profile1.getId(), profile2.getId());
-        priorityEdgeQueue.add(
-            new Edge(weight, profile1.getUser().getId(), profile2.getUser().getId()));
-      }
-    }
-    return priorityEdgeQueue;
-  }
-
-  private boolean checkBlock(Profile blockingProfile, Profile blockedProfile) {
-    return matchBlocker.isBlocked(blockedProfile, blockingProfile) ||
-        matchBlocker.isBlocked(blockingProfile, blockedProfile);
-  }
-
-  private int calculateWeight(Long fromProfileId, Long toProfileId) {
-    return preferenceCalculatingPolicy.calculatePreference(fromProfileId, toProfileId);
-  }
-
-  @AllArgsConstructor
-  private static class Edge implements Comparable<Edge> {
-
-    private int weight;
-    private Long user1Id;
-    private Long user2Id;
+    private final MatchService matchService;
+    private final MatchBlocker matchBlocker;
+    private final List<PreferenceCalculatingPolicy> preferenceCalculatingPolicyChain;
 
     @Override
-    public int compareTo(Edge o) {
-      return o.weight - this.weight;
+    @Transactional
+    public List<Profile> doMatch(List<Profile> profiles) {
+        PriorityQueue<Edge> priorityEdges = getPriorityEdges(profiles);
+        Set<Long> matchedSet = greedyMatch(priorityEdges);
+        List<Profile> unmatchedProfiles = new ArrayList<>();
+        for (Profile profile : profiles) {
+            if (!matchedSet.contains(profile.getUser().getId())) {
+                unmatchedProfiles.add(profile);
+            }
+        }
+        return unmatchedProfiles;
     }
-  }
+
+    private Set<Long> greedyMatch(PriorityQueue<Edge> priorityEdges) {
+        Set<Long> matchedSet = new HashSet<>();
+        while (!priorityEdges.isEmpty()) {
+            Edge edge = priorityEdges.poll();
+            if (matchedSet.contains(edge.user1Id) || matchedSet.contains(edge.user2Id)) {
+                continue;
+            }
+            matchedSet.add(edge.user1Id);
+            matchedSet.add(edge.user2Id);
+
+            matchService.createMatchInfo(edge.user1Id, edge.user2Id, false);
+        }
+        return matchedSet;
+    }
+
+    private PriorityQueue<Edge> getPriorityEdges(List<Profile> profiles) {
+        PriorityQueue<Edge> priorityEdgeQueue = new PriorityQueue<Edge>();
+        for (Profile profile1 : profiles) {
+            for (Profile profile2 : profiles) {
+                if (profile1.getId().equals(profile2.getId())) {
+                    continue;
+                }
+                if (checkBlock(profile1, profile2)) {
+                    continue;
+                }
+                int weight = calculateWeight(profile1.getId(), profile2.getId());
+                priorityEdgeQueue.add(
+                    new Edge(weight, profile1.getUser().getId(), profile2.getUser().getId()));
+            }
+        }
+        return priorityEdgeQueue;
+    }
+
+    private boolean checkBlock(Profile blockingProfile, Profile blockedProfile) {
+        return matchBlocker.isBlocked(blockedProfile, blockingProfile) ||
+            matchBlocker.isBlocked(blockingProfile, blockedProfile);
+    }
+
+    private int calculateWeight(Long fromProfileId, Long toProfileId) {
+        int weight = 0;
+        for (PreferenceCalculatingPolicy preferenceCalculatingPolicy : preferenceCalculatingPolicyChain) {
+            weight += preferenceCalculatingPolicy.calculatePreference(fromProfileId, toProfileId);
+        }
+        return weight;
+    }
+
+    @AllArgsConstructor
+    private static class Edge implements Comparable<Edge> {
+
+        private int weight;
+        private Long user1Id;
+        private Long user2Id;
+
+        @Override
+        public int compareTo(Edge o) {
+            return o.weight - this.weight;
+        }
+    }
 }
